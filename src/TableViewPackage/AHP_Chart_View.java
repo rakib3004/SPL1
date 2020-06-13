c55d4f6df597d7a0029b5310276adb18698b8f6f
rakib3004 <bsse1129@iit.du.ac.bd>
2020-06-13 11:23:54 +0600

    add new scatter chart and stacked area chart in  analytic hierarchy process chart view class

--- src/TableViewPackage/AHP_Chart_View.java
+++ src/TableViewPackage/AHP_Chart_View.java
@@ -17,10 +17,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
@@ -172,6 +169,291 @@ public class AHP_Chart_View extends Application {
         Canvas canvas = new Canvas(1500, 950);
         Group group = new Group();
         group.getChildren().addAll(canvas, lineChart, exit, back);
+
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        // graphicsContext.drawImage(image,0,0);
+
+
+        Scene scene1 = new Scene(group, 1500, 950);
+
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+    }
+
+ public void startScatterChart(Stage primaryStage) throws IOException {
+        String  className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        dateTimeWriter.dateTimeWriterMethods(className);
+
+        Button back = new Button("Back");
+        Button exit = new Button("Exit");
+        back.setTranslateX(0);
+        back.setTranslateY(650);
+        exit.setTranslateX(1100);
+        exit.setTranslateY(650);
+
+
+        back.setOnAction(actionEvent -> {
+            AnalyticHierarchyAlgorithmFx analyticHierarchyAlgorithmFx = new AnalyticHierarchyAlgorithmFx();
+
+            try {
+                analyticHierarchyAlgorithmFx.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+
+        });
+
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+
+        });
+
+
+        setStyle(exit);
+        setStyle(back);
+
+
+        back.setPrefSize(200, 80);
+        exit.setPrefSize(200, 80);
+
+
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
+        ahPcriteriaWeight =  ahPcalculation.AHPcalculationMethods(priorityData,numberOfBooks);
+        priorityData=     ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight,priorityData,numberOfBooks);
+        CategoryAxis categoryAxis = new CategoryAxis();
+        categoryAxis.setLabel("Book Class Category");
+
+        NumberAxis numberAxis = new NumberAxis();
+        numberAxis.setLabel("Numbers of Book");
+
+
+        ScatterChart scatterChart = new ScatterChart(categoryAxis, numberAxis);
+
+        XYChart.Series series1 = new XYChart.Series();
+
+
+        series1.setName("Book Weight Show");
+
+
+        int positionIndicator = 0;
+
+        //   series1.setName("Training Set 1 ");
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+
+            positionIndicator++;
+            series1.getData().add(new XYChart.Data(String.valueOf(positionIndicator), priorityData[iterator].getAHPweight()));
+
+        }
+
+
+        scatterChart.getData().add(series1);
+
+        scatterChart.setTranslateX(10);
+        scatterChart.setTranslateY(25);
+        scatterChart.setPrefSize(1350, 700);
+
+
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem mlr_process = new MenuItem("MLR Process");
+        MenuItem ahp_process = new MenuItem("AHP Process");
+        MenuItem pra_process = new MenuItem("PRA Process");
+
+
+
+        mlr_process.setOnAction((event) -> {
+            try {
+                MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
+                mlr_chart_view.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        ahp_process.setOnAction((event) -> {
+            AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
+            try {
+                ahp_chart_view.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        pra_process.setOnAction((event) -> {
+            PRA_Chart_View crossValidationVisualization = new PRA_Chart_View();
+            try {
+                crossValidationVisualization.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+        contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
+
+        scatterChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
+
+            @Override
+            public void handle(ContextMenuEvent event) {
+
+                contextMenu.show(scatterChart, event.getScreenX(), event.getScreenY());
+            }
+        });
+
+
+        //  Image image = new Image("libraryBackground9.jpg");
+        Canvas canvas = new Canvas(1500, 950);
+        Group group = new Group();
+        group.getChildren().addAll(canvas, scatterChart, exit, back);
+
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        // graphicsContext.drawImage(image,0,0);
+
+
+        Scene scene1 = new Scene(group, 1500, 950);
+
+
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+    }
+
+
+public void startStackedAreaChart(Stage primaryStage) throws IOException {
+        String  className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        dateTimeWriter.dateTimeWriterMethods(className);
+
+        Button back = new Button("Back");
+        Button exit = new Button("Exit");
+        back.setTranslateX(0);
+        back.setTranslateY(650);
+        exit.setTranslateX(1100);
+        exit.setTranslateY(650);
+
+
+        back.setOnAction(actionEvent -> {
+            AnalyticHierarchyAlgorithmFx analyticHierarchyAlgorithmFx = new AnalyticHierarchyAlgorithmFx();
+
+            try {
+                analyticHierarchyAlgorithmFx.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+
+        });
+
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+
+        });
+
+
+        setStyle(exit);
+        setStyle(back);
+
+
+        back.setPrefSize(200, 80);
+        exit.setPrefSize(200, 80);
+
+
+        priorityData = processing.fileReaderMethods();
+        numberOfBooks = bookNumber.bookNumberFindingMethods();
+        ahPcriteriaWeight =  ahPcalculation.AHPcalculationMethods(priorityData,numberOfBooks);
+        priorityData=     ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight,priorityData,numberOfBooks);
+        CategoryAxis categoryAxis = new CategoryAxis();
+        categoryAxis.setLabel("Book Class Category");
+
+        NumberAxis numberAxis = new NumberAxis();
+        numberAxis.setLabel("Numbers of Book");
+
+
+        StackedAreaChart stackedAreaChart = new StackedAreaChart(categoryAxis, numberAxis);
+
+        XYChart.Series series1 = new XYChart.Series();
+
+
+        series1.setName("Book Weight Show");
+
+
+        int positionIndicator = 0;
+
+        //   series1.setName("Training Set 1 ");
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+
+            positionIndicator++;
+            series1.getData().add(new XYChart.Data(String.valueOf(positionIndicator), priorityData[iterator].getAHPweight()));
+
+        }
+
+
+        stackedAreaChart.getData().add(series1);
+
+        stackedAreaChart.setTranslateX(10);
+        stackedAreaChart.setTranslateY(25);
+        stackedAreaChart.setPrefSize(1350, 700);
+
+
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem mlr_process = new MenuItem("MLR Process");
+        MenuItem ahp_process = new MenuItem("AHP Process");
+        MenuItem pra_process = new MenuItem("PRA Process");
+
+
+
+        mlr_process.setOnAction((event) -> {
+            try {
+                MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
+                mlr_chart_view.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+        ahp_process.setOnAction((event) -> {
+            AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
+            try {
+                ahp_chart_view.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        pra_process.setOnAction((event) -> {
+            PRA_Chart_View crossValidationVisualization = new PRA_Chart_View();
+            try {
+                crossValidationVisualization.start(primaryStage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+
+
+        contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
+
+        stackedAreaChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
+
+            @Override
+            public void handle(ContextMenuEvent event) {
+
+                contextMenu.show(stackedAreaChart, event.getScreenX(), event.getScreenY());
+            }
+        });
+
+
+        //  Image image = new Image("libraryBackground9.jpg");
+        Canvas canvas = new Canvas(1500, 950);
+        Group group = new Group();
+        group.getChildren().addAll(canvas, stackedAreaChart, exit, back);
 
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         // graphicsContext.drawImage(image,0,0);
