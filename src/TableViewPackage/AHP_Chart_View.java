ab42742993bb5619b86fafb81a64d3b10fb88551
rakib3004 <bsse1129@iit.du.ac.bd>
2020-06-13 12:54:14 +0600

    add new scatter and stacked area chart in Page rank Algorithm process chart view class

--- src/TableViewPackage/AHP_Chart_View.java
+++ src/TableViewPackage/AHP_Chart_View.java
@@ -285,9 +285,9 @@ public class AHP_Chart_View extends Application {
             }
         });
         pra_process.setOnAction((event) -> {
-            PRA_Chart_View crossValidationVisualization = new PRA_Chart_View();
+            PRA_Chart_View pra_chart_view = new PRA_Chart_View();
             try {
-                crossValidationVisualization.start(primaryStage);
+                pra_chart_view.start(primaryStage);
             } catch (IOException e) {
                 e.printStackTrace();
             }
