bee58deefc6a4794029c292b788fad7fc3fb7636
Md. Rakib Trofder <48678659+rakib3004@users.noreply.github.com>
2020-06-21 02:10:45 +1200

    add noAccessCounter in DateTimeWriter class

--- src/FilePackage/DateTimeWriter.java
+++ src/FilePackage/DateTimeWriter.java
@@ -7,19 +7,24 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTimeWriter {
+
+    int classCounter=0;
     public void dateTimeWriterInitialMethods(String className){
+
         Date dNow = new Date( );
         SimpleDateFormat ft =
                 new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a");
 
-String FILENAME,string;
+String FILENAME,string,COUNTER_FILE;
         FILENAME = "history.txt";
+        COUNTER_FILE="ClassCounter.txt";
 
-     string =  className+"\t";
+
+        string =  className+"\t";
         string = string+ ft.format(dNow)+"\n";
 
-     BufferedWriter out = null;
-         /*   try {
+    /*   BufferedWriter out = null;
+          try {
                 out = new BufferedWriter(
                         new FileWriter(FILENAME, true));
             } catch (IOException e) {
@@ -38,6 +43,19 @@ String FILENAME,string;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        classCounter++;
+        String noAccessClass = Integer.toString(classCounter);
+
+        FileWriter fileWriter1 = null;
+        try {
+            fileWriter1 = new FileWriter(COUNTER_FILE);
+            fileWriter1.write(noAccessClass);
+            fileWriter1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
     public void dateTimeWriterMethods(String className){
@@ -71,6 +89,20 @@ String FILENAME,string;
         } catch (IOException e) {
             e.printStackTrace();
         }*/
+
+   String  COUNTER_FILE="ClassCounter.txt";
+        classCounter++;
+        String noAccessClass = Integer.toString(classCounter);
+
+        FileWriter fileWriter1 = null;
+        try {
+            fileWriter1 = new FileWriter(COUNTER_FILE);
+            fileWriter1.write(noAccessClass);
+            fileWriter1.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
