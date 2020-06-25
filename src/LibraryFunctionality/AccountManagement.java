8bb34192a1ef9f967babda7efc753017bde7a972
rakib3004 <bsse1129@iit.du.ac.bd>
2020-06-25 20:24:30 +0600

    add new User Profile AND Counter to count member in AccountManagement.java

--- src/LibraryFunctionality/AccountManagement.java
+++ src/LibraryFunctionality/AccountManagement.java
@@ -1,7 +1,10 @@
 package LibraryFunctionality;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Parameter;
 
 public class AccountManagement {
     public void accountManagementMethods(String [] infoDataArray , String [] addFavouriteBookType) throws IOException {
@@ -17,10 +20,39 @@ public class AccountManagement {
                 File file2 = new File("UserProfile"+File.separator+FILENAME);
                 file2.createNewFile();
             }
-            file1.createNewFile();
+            else{
+                file1.createNewFile();
+
+            }
         }
         else {
             file.createNewFile();
+String FILENAME_COUNT = "LibraryMemberCount.txt";
+File fileA = new File(FILENAME_COUNT);
+            FileReader fileReader = new FileReader(fileA);
+            char [] libraryMembers = new char[10];
+            fileReader.read(libraryMembers);
+            String  stringMemebers="";
+            int countIterator=0;
+            for(countIterator=0;libraryMembers[countIterator]!='\0';countIterator++){
+                stringMemebers = stringMemebers+libraryMembers[countIterator];
+
+            }
+            stringMemebers = stringMemebers.trim();
+            int intMembers = Integer.parseInt(stringMemebers);
+            intMembers++;
+            int userID = 1000+intMembers;
+            String textUserID = Integer.toString(userID);
+
+
+            try {
+                FileWriter fileWriter=new FileWriter(FILENAME);
+                fileWriter.write(infoDataArray[0]+"\t"+textUserID);
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
 
         }
     }
