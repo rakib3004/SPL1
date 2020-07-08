3566f4fd984001e2428e96b4cec1f36595a3b9ee
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-08 23:47:41 +0600

    remove space and add comment in AHPcalculation.java

--- src/AHPalgorithm/AHPcalculation.java
+++ src/AHPalgorithm/AHPcalculation.java
@@ -4,9 +4,6 @@ import FilePackage.DateTimeWriter;
 import ObjectOriented.AHPcriteriaWeight;
 import ObjectOriented.PriorityData;
 import Regression.newVersion.*;
-
-
-
 public class AHPcalculation {
     AHPcriteriaWeight ahPcriteriaWeight;
     TypeCountRegression typeCountRegression = new TypeCountRegression();
@@ -15,7 +12,6 @@ public class AHPcalculation {
     CountMonthRegression countMonthRegression = new CountMonthRegression();
     CountPriceRegression countPriceRegression = new CountPriceRegression();
     MonthPriceRegression  monthPriceRegression = new MonthPriceRegression();
-
     AHPcriteriaWeight ahPcriteriaWeight4,ahPcriteriaWeight2,ahPcriteriaWeight3, ahPcriteriaWeight1;
     AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
     TypeCriteriaAHP typeCriteriaAHP = new TypeCriteriaAHP();
@@ -31,8 +27,8 @@ public class AHPcalculation {
 
         double [][] AHPMatrix = new double [4] [4];
 
-
         int i,j;
+        //assume priority among types by linear regression but which is not efficient
         AHPMatrix[0][1] = typeCountRegression.typeCountRegressionMethods(priorityData,numberOfBooks);
         AHPMatrix[0][2] = typeMonthRegression.typeMonthRegressionMethods(priorityData,numberOfBooks);
         AHPMatrix[0][3] = typePriceRegression.typePriceRegressionMethods(priorityData,numberOfBooks);
@@ -41,7 +37,6 @@ public class AHPcalculation {
         AHPMatrix[2][3] = monthPriceRegression.monthPriceRegressionMethods(priorityData,numberOfBooks);
 
         // new methodology for analytical hierarchy process.
-
         AHPMatrix[0][1] = 2.0;
         AHPMatrix[0][2] = 3.0;
         AHPMatrix[0][3] = 5.0;
@@ -49,13 +44,12 @@ public class AHPcalculation {
         AHPMatrix[1][3] = 4.0;
         AHPMatrix[2][3] = 1.5;
 
-
+        //calculate by matrix calculation according to Analytic Hierarchy Process Algorithm
         for(i=0;i<4;i++){
             for(j=i+1;j<4;j++){
                 AHPMatrix[i][j]=Math.abs(AHPMatrix[i][j]);
             }
         }
-
         for(i=0;i<4;i++){
             for(j=i+1;j<4;j++){
 
@@ -63,14 +57,8 @@ public class AHPcalculation {
             }
         }
         for(i=0;i<4;i++){
-
                 AHPMatrix[i][i]=(1);
-
         }
-
-
-
-
         double [] summationMatrix = new double[4];
 
         for(i=0;i<4;i++){
@@ -96,17 +84,14 @@ weightMatrix[i] = weightMatrix[i] + AHPMatrix[j][i];
             }
             weightMatrix[i] = weightMatrix[i]/4;
         }
-
-
-
+//put the value into another class to make an object for use them for all other operatio
 
         ahPcriteriaWeight4 =    typeCriteriaAHP.typeCriteriaAHPMethods(weightMatrix[0],priorityData,numberOfBooks);
         ahPcriteriaWeight2=    countCriteriaAHP.countCriteriaAHPMethods(weightMatrix[1],priorityData,numberOfBooks);
         ahPcriteriaWeight3=   monthCriteriaAHP.monthCriteriaAHPMethods(weightMatrix[2],priorityData,numberOfBooks);
         ahPcriteriaWeight1 =    priceCriteriaAHP.priceCriteriaAHPMethods(weightMatrix[3],priorityData,numberOfBooks);
 
-
-
+        // pass all variables through a AHP criteria constructor
          ahPcriteriaWeight = new AHPcriteriaWeight(ahPcriteriaWeight1.highPrice, ahPcriteriaWeight1.mediumPrice,
                 ahPcriteriaWeight1.lowPrice,ahPcriteriaWeight2.highlyDemand,
                 ahPcriteriaWeight2.highMediumDemand,ahPcriteriaWeight2.lowMediumDemand,
@@ -116,7 +101,6 @@ weightMatrix[i] = weightMatrix[i] + AHPMatrix[j][i];
                 ahPcriteriaWeight4.uponnashType, ahPcriteriaWeight4.kobitaType,
                 ahPcriteriaWeight4.scienceFictionType, ahPcriteriaWeight4.scienceFictionType,
                 ahPcriteriaWeight4.kisorUponnashType, ahPcriteriaWeight4.othersType);
-
 
                 return  ahPcriteriaWeight;
 
