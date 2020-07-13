1da9275c1ccf9e744981e01f82c4d8e5ef7e3751
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-13 16:03:02 +0600

    remove shortcurt buttons in CrossValidationFX.java and TestingSet.java

--- src/CrossValidationProcess/TestingSet.java
+++ src/CrossValidationProcess/TestingSet.java
@@ -112,44 +112,6 @@ public class TestingSet extends Application {
         group.getChildren().addAll(canvas,exit,back,testingSetResults,
                 trainingTestingCompersion,codeValidationSet);
 
-        Button administrationMood = new Button("Administration Mood");
-        Button readersMood = new Button("Readers Mood");
-        Button about = new Button("About");
-        readersMood.setTranslateX(20);
-        readersMood.setTranslateY(260);
-        administrationMood.setTranslateX(20);
-        administrationMood.setTranslateY(320);
-        about.setTranslateX(20);
-        about.setTranslateY(380);
-        administrationMood.setPrefSize(160, 30);
-        readersMood.setPrefSize(160, 30);
-        about.setPrefSize(160,30);
-        administrationMood.setOnAction(actionEvent -> {
-
-            FXThirdWindow fxThirdWindow = new FXThirdWindow();
-            try {
-                fxThirdWindow.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        readersMood.setOnAction(actionEvent -> {
-            ReadingRoom readingRoom = new ReadingRoom();
-            try {
-                readingRoom.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        about.setOnAction(actionEvent -> {
-            About about1 = new About();
-            try {
-                about1.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        group.getChildren().addAll(readersMood,administrationMood,about);
 
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         graphicsContext.drawImage(image,0,0);
