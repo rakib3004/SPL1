01034d1a87ef6e654bdbcaae66d33a7a6b62306a
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-22 16:44:53 +0600

    add new button design in PageRankAlgorithm.java

--- src/RecommendationAlgorithm/PageRankAlgorithm.java
+++ src/RecommendationAlgorithm/PageRankAlgorithm.java
@@ -9,6 +9,8 @@ import CrossValidationProcess.TrainingSet;
 import FilePackage.DateTimeWriter;
 import InfoDisplay.SevenValueStatistics;
 import InfoDisplay.BookInformationShow;
+import JavFX.AuthorSystem;
+import JavFX.Main;
 import JavFX.ProcessAnalysis;
 import LibraryFunctionality.ReadingRoom;
 import MainPackage.BookNumber;
@@ -18,6 +20,8 @@ import ObjectOriented.PriorityData;
 import PageRankAlgorithm.PageRankCalculation;
 import PageRankAlgorithm.PageRankProcessData;
 import RegressionFx.FourVariableRegression;
+import TableViewPackage.AHP_Chart_View;
+import TableViewPackage.AHP_TableView;
 import TableViewPackage.PRA_Chart_View;
 import TableViewPackage.PRA_TableView;
 import UserInterfacePackage.AddBook;
@@ -30,12 +34,17 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -55,21 +64,13 @@ public class PageRankAlgorithm extends Application {
         DateTimeWriter dateTimeWriter =  new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
-        Button consoleView = new Button("Console View");
+      /*  Button consoleView = new Button("Console View");
         Button back = new Button("Back");
         Button exit = new Button("Exit");
         Button tableView = new Button("Table View");
         tableView.setTranslateX(860);
         tableView.setTranslateY(50);
-        tableView.setOnAction(actionEvent -> {
-            PRA_TableView pra_tableView = new PRA_TableView();
-            try {
-                pra_tableView.start(primaryStage);
-            }
-            catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
+
         setStyle(tableView);
         tableView.setPrefSize(350,80);
         consoleView.setTranslateX(140);
@@ -77,21 +78,7 @@ public class PageRankAlgorithm extends Application {
         back.setTranslateX(0);
         back.setTranslateY(650);
         exit.setTranslateX(1100);
-        exit.setTranslateY(650);
-        consoleView.setOnAction(actionEvent -> {
-            PageRankCalculation pageRankCalculation = new PageRankCalculation();
-            try {
-                priorityData = processing.fileReaderMethods();
-                numberOfBooks = bookNumber.bookNumberFindingMethods();
-                //   priorityData = pageRankCalculation.pageRankCalculationMethods(priorityData,numberOfBooks);
-                PageRankProcessData pageRankProcessData = new PageRankProcessData();
-                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);                System.exit(0);
-                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);                System.exit(0);
-            }
-            catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
+
         back.setOnAction(actionEvent -> {
             ProcessImplementation processImplementation = new ProcessImplementation();
 
@@ -113,6 +100,74 @@ public class PageRankAlgorithm extends Application {
         Button graphView = new Button("Graph View");
         graphView.setTranslateX(500);
         graphView.setTranslateY(50);
+
+        setStyle(graphView);
+        graphView.setPrefSize(200,80);
+      //  Image image = new Image("Images"+ File.separator +"libraryBackground6.jpg");
+        Canvas canvas = new Canvas(1500,950);
+        Group group = new Group();
+        group.getChildren().addAll(canvas,tableView,consoleView,graphView,exit,back);*/
+        Font font = new Font(19);
+        Font font1 = new Font(47);
+        Font font2 = new Font( 26);
+        Button tableView = new Button("Table View");
+        Button graphView = new Button("Graph View");
+        Button consoleView = new Button("Console View");
+
+        tableView.setPrefSize(410,230);
+        graphView.setPrefSize(410,230);
+        consoleView.setPrefSize(410,230);
+
+        tableView.setFont(font2);
+        graphView.setFont(font2);
+        consoleView.setFont(font2);
+        tableView.setTranslateX(30);
+        tableView.setTranslateY(35);
+        graphView.setTranslateX(30);
+        graphView.setTranslateY(270);
+        consoleView.setTranslateX(30);
+        consoleView.setTranslateY(505);
+
+        tableView.setContentDisplay(ContentDisplay.TOP);
+        graphView.setContentDisplay(ContentDisplay.TOP);
+        consoleView.setContentDisplay(ContentDisplay.TOP);
+        FileInputStream fileInputStream1 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"table.png");
+        Image image1 = new Image(fileInputStream1);
+        tableView.setGraphic(new ImageView(image1));
+
+        FileInputStream fileInputStream2 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"graph.png");
+        Image image2 = new Image(fileInputStream2);
+        graphView.setGraphic(new ImageView(image2));
+
+        FileInputStream fileInputStream3 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"console.png");
+        Image image3 = new Image(fileInputStream3);
+        consoleView.setGraphic(new ImageView(image3));
+        tableView.setOnAction(actionEvent -> {
+            PRA_TableView pra_tableView = new PRA_TableView();
+            try {
+                pra_tableView.start(primaryStage);
+            }
+            catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        consoleView.setOnAction(actionEvent -> {
+            PageRankCalculation pageRankCalculation = new PageRankCalculation();
+            try {
+                priorityData = processing.fileReaderMethods();
+                numberOfBooks = bookNumber.bookNumberFindingMethods();
+                //   priorityData = pageRankCalculation.pageRankCalculationMethods(priorityData,numberOfBooks);
+                PageRankProcessData pageRankProcessData = new PageRankProcessData();
+                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);                System.exit(0);
+                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);                System.exit(0);
+            }
+            catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
         graphView.setOnAction(actionEvent -> {
             PRA_Chart_View pra_chart_view = new PRA_Chart_View();
             try {
@@ -125,12 +180,74 @@ public class PageRankAlgorithm extends Application {
             }
 
         });
-        setStyle(graphView);
-        graphView.setPrefSize(200,80);
-      //  Image image = new Image("Images"+ File.separator +"libraryBackground6.jpg");
-        Canvas canvas = new Canvas(1500,950);
+
+        Button exit = new Button("Exit");
+        exit.setTranslateX(1200);
+        exit.setTranslateY(700);
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+        });
+        FileInputStream fileInputStream4 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"exit.png");
+        Image image4 = new Image(fileInputStream4);
+        exit.setGraphic(new ImageView(image4));
+        exit.setPrefSize(120, 20);
+        exit.setContentDisplay(ContentDisplay.LEFT);
+        exit.setFont(font);
+        Text text = new Text("Process Implementation");
+        text.setTranslateX(600);
+        text.setTranslateY(500);
+        text.setFont(font1);
+        text.setFill(Color.BLACK);
+        text.setTextAlignment(TextAlignment.LEFT);
+        text.setStyle("-fx-font-weight: bold;");
+
+        Button home = new Button("Home");
+        home.setTranslateX(470);
+        home.setTranslateY(520);
+        home.setPrefSize(375,30);
+        home.setFont(font2);
+        home.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream5 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"home.png");
+        Image image5 = new Image(fileInputStream5);
+        home.setGraphic(new ImageView(image5));
+        home.setOnAction(actionEvent -> {
+            Main main = new Main();
+            try {
+                main.start(primaryStage);
+            }
+            catch (Exception exception){
+                exception.printStackTrace();
+            }
+        });
+        Button back = new Button("Back");
+        back.setTranslateX(950);
+        back.setTranslateY(520);
+        back.setPrefSize(375,30);
+        back.setFont(font2);
+        back.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream6 = new FileInputStream(
+                "src"+ File.separator +"Images"+ File.separator +"back.png");
+        Image image6 = new Image(fileInputStream6);
+        back.setGraphic(new ImageView(image6));
+        back.setOnAction(actionEvent -> {
+            AuthorSystem authorSystem = new AuthorSystem();
+            try {
+                authorSystem.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Image background = new Image("Images"+ File.separator +"framework.jpg");
+        Canvas canvas = new Canvas(850, 425);
+        canvas.setTranslateX(470);
+        canvas.setTranslateY(35);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.drawImage(background,0,0);
         Group group = new Group();
-        group.getChildren().addAll(canvas,tableView,consoleView,graphView,exit,back);
+        group.getChildren().addAll(canvas,tableView,
+                graphView,exit,back,home,text,consoleView);
         ReadingRoom readingRoom2 = new ReadingRoom();
         LibraryDesk libraryDesk2 = new LibraryDesk();
         CrossValidation crossValidation2 = new CrossValidation();
@@ -391,7 +508,6 @@ public class PageRankAlgorithm extends Application {
         menuBar.setStyle(" -fx-spacing: 47;");
         menuBar.prefHeight(32);
         group.getChildren().add(menuBar);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
       //  graphicsContext.drawImage(image,0,0);
         Scene scene1 = new Scene(group,1500,950);
         primaryStage.setScene(scene1);
