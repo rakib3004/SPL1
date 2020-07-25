7413c2b45514e70e268694e30b94d36c52066174
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-25 12:53:33 +0600

    add new design in SevenNumberImplementation.priceStatistics

--- src/InfoDisplay/SevenNumberImplementation.java
+++ src/InfoDisplay/SevenNumberImplementation.java
@@ -575,22 +575,151 @@ StackedArea stackedArea = new StackedArea();
          DateTimeWriter dateTimeWriter =  new DateTimeWriter();
          dateTimeWriter.dateTimeWriterMethods(className);
 
-         Button back = new Button("Back");
-         Button exit = new Button("Exit");
+         Font font = new Font(19);
+         Font font1 = new Font(47);
+         Font font2 = new Font( 26);
+
          Button scatterChart = new Button("Scatter Chart");
          Button lineChart = new Button("Line Chart");
          Button stackedAreaChart = new Button("Stacked Area Chart");
-         back.setTranslateX(0);
-         back.setTranslateY(650);
-         exit.setTranslateX(1100);
-         exit.setTranslateY(650);
 
-         scatterChart.setTranslateX(500);
-         scatterChart.setTranslateY(200);
-         lineChart.setTranslateX(500);
-         lineChart.setTranslateY(300);
-         stackedAreaChart.setTranslateX(500);
-         stackedAreaChart.setTranslateY(400);
+         scatterChart.setPrefSize(410,230);
+         lineChart.setPrefSize(410,230);
+         stackedAreaChart.setPrefSize(410,230);
+
+         scatterChart.setFont(font);
+         lineChart.setFont(font);
+         stackedAreaChart.setFont(font2);
+         scatterChart.setTranslateX(30);
+         scatterChart.setTranslateY(35);
+         lineChart.setTranslateX(30);
+         lineChart.setTranslateY(270);
+         stackedAreaChart.setTranslateX(30);
+         stackedAreaChart.setTranslateY(505);
+
+         scatterChart.setContentDisplay(ContentDisplay.TOP);
+         lineChart.setContentDisplay(ContentDisplay.TOP);
+         stackedAreaChart.setContentDisplay(ContentDisplay.TOP);
+         FileInputStream fileInputStream1 = null;
+         try {
+             fileInputStream1 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"cross.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image1 = new Image(fileInputStream1);
+         scatterChart.setGraphic(new ImageView(image1));
+
+         FileInputStream fileInputStream2 = null;
+         try {
+             fileInputStream2 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"7value.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image2 = new Image(fileInputStream2);
+         lineChart.setGraphic(new ImageView(image2));
+
+         FileInputStream fileInputStream3 = null;
+         try {
+             fileInputStream3 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"optimization.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image3 = new Image(fileInputStream3);
+         stackedAreaChart.setGraphic(new ImageView(image3));
+         scatterChart.setOnAction(actionEvent -> {
+             try {
+                 this.scatterChart.startBorrowing(priceStatisticsStage);
+
+             } catch (Exception exception) {
+                 exception.printStackTrace();
+             }
+
+         });
+         stackedAreaChart.setOnAction(actionEvent -> {
+             try {
+                 stackedArea.startBorrowing(priceStatisticsStage);
+
+             } catch (Exception exception) {
+                 exception.printStackTrace();
+             }
+         });
+
+         lineChart.setOnAction(actionEvent -> {
+             try {
+                 this.lineChart.startBorrowing(priceStatisticsStage);
+
+             } catch (Exception exception) {
+                 exception.printStackTrace();
+             }
+         });
+         Button exit = new Button("Exit");
+         exit.setTranslateX(1200);
+         exit.setTranslateY(700);
+         exit.setOnAction(actionEvent -> {
+             System.exit(0);
+         });
+         FileInputStream fileInputStream4 = null;
+         try {
+             fileInputStream4 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"exit.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image4 = new Image(fileInputStream4);
+         exit.setGraphic(new ImageView(image4));
+         exit.setPrefSize(120, 20);
+         exit.setContentDisplay(ContentDisplay.LEFT);
+         exit.setFont(font);
+         Text text = new Text("Process Analysis");
+         text.setTranslateX(575);
+         text.setTranslateY(500);
+         text.setFont(font1);
+         text.setFill(Color.BLACK);
+         text.setTextAlignment(TextAlignment.LEFT);
+         text.setStyle("-fx-font-weight: bold;");
+
+         Button home = new Button("Home");
+         home.setTranslateX(470);
+         home.setTranslateY(520);
+         home.setPrefSize(375,30);
+         home.setFont(font2);
+         home.setContentDisplay(ContentDisplay.LEFT);
+         FileInputStream fileInputStream5 = null;
+         try {
+             fileInputStream5 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"home.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image5 = new Image(fileInputStream5);
+         home.setGraphic(new ImageView(image5));
+         home.setOnAction(actionEvent -> {
+             Main main = new Main();
+             try {
+                 main.start(priceStatisticsStage);
+             }
+             catch (Exception exception){
+                 exception.printStackTrace();
+             }
+         });
+         Button back = new Button("Back");
+         back.setTranslateX(950);
+         back.setTranslateY(520);
+         back.setPrefSize(375,30);
+         back.setFont(font2);
+         back.setContentDisplay(ContentDisplay.LEFT);
+         FileInputStream fileInputStream6 = null;
+         try {
+             fileInputStream6 = new FileInputStream(
+                     "src"+ File.separator +"Images"+ File.separator +"back.png");
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+         Image image6 = new Image(fileInputStream6);
+         back.setGraphic(new ImageView(image6));
          back.setOnAction(actionEvent -> {
              SevenValueStatistics sevenValueStatistics = new SevenValueStatistics();
              try {
@@ -599,47 +728,18 @@ StackedArea stackedArea = new StackedArea();
                  exception.printStackTrace();
              }
          });
-         exit.setOnAction(actionEvent -> {
-             System.exit(0);
-         });
-         scatterChart.setOnAction(actionEvent -> {
-             try {
-                 this.scatterChart.startPricing(priceStatisticsStage);
-             } catch (Exception exception) {
-                 exception.printStackTrace();
-             }
-         });
-         stackedAreaChart.setOnAction(actionEvent -> {
-             try {
-                 stackedArea.startPricing(priceStatisticsStage);
-             } catch (Exception exception) {
-                 exception.printStackTrace();
-             }
-         });
-         lineChart.setOnAction(actionEvent -> {
-             try {
-                 this.lineChart.startPricing(priceStatisticsStage);
-             } catch (Exception exception) {
-                 exception.printStackTrace();
-             }
-         });
-         setStyle(exit);
-         setStyle(back);
-         setStyle(lineChart);
-         setStyle(scatterChart);
-         setStyle(stackedAreaChart);
-         back.setPrefSize(200, 80);
-         exit.setPrefSize(200, 80);
-         lineChart.setPrefSize(275, 80);
-         scatterChart.setPrefSize(275, 80);
-         stackedAreaChart.setPrefSize(275, 80);
-
-         Image image = new Image("Images"+ File.separator +"libraryBackground17.jpg");
-         Canvas canvas = new Canvas(1500,950);
-         Group group = new Group();
-         group.getChildren().addAll(canvas,exit,back,scatterChart,lineChart,stackedAreaChart);
+         Image background = new Image("Images"+ File.separator +"framework.jpg");
+         Canvas canvas = new Canvas(850, 425);
+         canvas.setTranslateX(470);
+         canvas.setTranslateY(35);
          GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-         graphicsContext.drawImage(image,0,0);
+         graphicsContext.drawImage(background,0,0);
+         Group group = new Group();
+         group.getChildren().addAll(canvas,scatterChart,
+                 lineChart,
+                 stackedAreaChart,exit,home,text,back);
+
+
          Scene scene1 = new Scene(group,1500,950);
 
          priceStatisticsStage.setScene(scene1);
