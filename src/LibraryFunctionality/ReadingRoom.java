e604e9f9adc5400f1271c2105ab72cbcdd0e9102
rakib3004 <bsse1129@iit.du.ac.bd>
2020-07-28 00:40:37 +0600

    add new design in ReadingRoom.addFavouriteBookType

--- src/LibraryFunctionality/ReadingRoom.java
+++ src/LibraryFunctionality/ReadingRoom.java
@@ -891,7 +891,7 @@ JOptionPane.showMessageDialog(null,"Please Choose Your\nFavourite Writer");
         primaryStage.show();
     }
     public void automaticSystem(Stage primaryStage) throws IOException {
-        String  className = this.getClass().getSimpleName();
+      /*  String  className = this.getClass().getSimpleName();
         DateTimeWriter dateTimeWriter =  new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         Button writerWise = new Button("Writer Wise");
@@ -1261,6 +1261,486 @@ JOptionPane.showMessageDialog(null,"Please Choose Your\nFavourite Writer");
         Scene scene1 = new Scene(group,1500,950);
         menuBar.prefWidthProperty().bind(scene1.widthProperty());
 
+        primaryStage.setScene(scene1);
+        primaryStage.setTitle("Books Statistics");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();*/
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
+        dateTimeWriter.dateTimeWriterMethods(className);
+        Font font = new Font(19);
+        Font font1 = new Font(47);
+        Font font2 = new Font(23);
+
+        Button writerWise = new Button("Writer Wise");
+        Button typeWise1 = new Button("Type Wise");
+        Button selfWise = new Button("Self Wise");
+        Button timeWise = new Button("Time Wise");
+
+        typeWise1.setPrefSize(400, 170);
+        selfWise.setPrefSize(400, 170);
+        timeWise.setPrefSize(400, 170);
+        writerWise.setPrefSize(400, 170);
+
+        typeWise1.setFont(font2);
+        selfWise.setFont(font2);
+        timeWise.setFont(font2);
+        writerWise.setFont(font2);
+
+        typeWise1.setTranslateX(30);
+        typeWise1.setTranslateY(35);
+        selfWise.setTranslateX(30);
+        selfWise.setTranslateY(210);
+        timeWise.setTranslateX(30);
+        timeWise.setTranslateY(385);
+        writerWise.setTranslateX(30);
+        writerWise.setTranslateY(560);
+
+        typeWise1.setContentDisplay(ContentDisplay.LEFT);
+        selfWise.setContentDisplay(ContentDisplay.LEFT);
+        timeWise.setContentDisplay(ContentDisplay.LEFT);
+        writerWise.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream1 = null;
+        try {
+            fileInputStream1 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "types.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image1 = new Image(fileInputStream1);
+        typeWise1.setGraphic(new ImageView(image1));
+
+        FileInputStream fileInputStream2 = null;
+        try {
+            fileInputStream2 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "time.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image2 = new Image(fileInputStream2);
+        selfWise.setGraphic(new ImageView(image2));
+
+        FileInputStream fileInputStream3 = null;
+        try {
+            fileInputStream3 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "borrow.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image3 = new Image(fileInputStream3);
+        timeWise.setGraphic(new ImageView(image3));
+
+        FileInputStream fileInputStream7 = null;
+        try {
+            fileInputStream7 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "demand.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image7 = new Image(fileInputStream7);
+        writerWise.setGraphic(new ImageView(image7));
+
+
+        writerWise.setOnAction(actionEvent -> {
+            try {
+
+                WriterWise writerWiseFX = new WriterWise();
+                writerWiseFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        typeWise1.setOnAction(actionEvent -> {
+
+            try {
+                TypeWise typeWise = new TypeWise();
+                typeWise.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        selfWise.setOnAction(actionEvent -> {
+
+            try {
+                RemoveBook.SelfWiseFX selfWiseFX = new RemoveBook.SelfWiseFX();
+                selfWiseFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        timeWise.setOnAction(actionEvent -> {
+            try {
+                TimeWise timeWiseFX = new TimeWise();
+                timeWiseFX.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+
+
+        Button exit = new Button("Exit");
+        exit.setTranslateX(1200);
+        exit.setTranslateY(700);
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+        });
+        FileInputStream fileInputStream4 = null;
+        try {
+            fileInputStream4 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "exit.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image4 = new Image(fileInputStream4);
+        exit.setGraphic(new ImageView(image4));
+        exit.setPrefSize(120, 20);
+        exit.setContentDisplay(ContentDisplay.LEFT);
+        exit.setFont(font);
+        Text text = new Text("Seven Value Statistics");
+        text.setTranslateX(575);
+        text.setTranslateY(500);
+        text.setFont(font1);
+        text.setFill(Color.BLACK);
+        text.setTextAlignment(TextAlignment.LEFT);
+        text.setStyle("-fx-font-weight: bold;");
+
+        Button home = new Button("Home");
+        home.setTranslateX(470);
+        home.setTranslateY(520);
+        home.setPrefSize(375, 30);
+        home.setFont(font2);
+        home.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream5 = null;
+        try {
+            fileInputStream5 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "home.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image5 = new Image(fileInputStream5);
+        home.setGraphic(new ImageView(image5));
+        home.setOnAction(actionEvent -> {
+            Main main = new Main();
+            try {
+                main.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Button back = new Button("Back");
+        back.setTranslateX(950);
+        back.setTranslateY(520);
+        back.setPrefSize(375, 30);
+        back.setFont(font2);
+        back.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream6 = null;
+        try {
+            fileInputStream6 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "back.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image6 = new Image(fileInputStream6);
+        back.setGraphic(new ImageView(image6));
+        back.setOnAction(actionEvent -> {
+            AuthorSystem authorSystem = new AuthorSystem();
+            try {
+                authorSystem.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Image background = new Image("Images" + File.separator + "framework.jpg");
+        Canvas canvas = new Canvas(850, 425);
+        canvas.setTranslateX(470);
+        canvas.setTranslateY(35);
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        graphicsContext.drawImage(background, 0, 0);
+        Group group = new Group();
+        group.getChildren().addAll(canvas, typeWise1,
+                selfWise, writerWise,
+                timeWise, exit, home, text, back);
+        ReadingRoom readingRoom2 = new ReadingRoom();
+        LibraryDesk libraryDesk2 = new LibraryDesk();
+        CrossValidation crossValidation2 = new CrossValidation();
+        SevenValueStatistics sevenValueStatistics1 = new SevenValueStatistics();
+        WriterCollection writerCollection = new WriterCollection();
+        FourVariableRegression fourVariableRegression1 = new FourVariableRegression();
+        AnalyticHierarchyAlgorithm analyticHierarchyAlgorithm1 = new AnalyticHierarchyAlgorithm();
+        PageRankAlgorithm pageRankAlgorithm1 = new PageRankAlgorithm();
+        AddBook addBook = new AddBook();
+        RemoveBook removeBook = new RemoveBook();
+        BookInformationShow bookInformationShow = new BookInformationShow();
+
+        Menu menu1 = new Menu("User");
+        Menu menu2 = new Menu("Librarian");
+        Menu menu3 = new Menu("Analysis");
+        Menu menu4 = new Menu("Process");
+        Menu menu5 = new Menu("Shortcut");
+
+        MenuItem menuItem1a = new MenuItem("Default Recommendation");
+        MenuItem menuItem1b = new MenuItem("User Based Recommendation");
+        menu1.getItems().addAll(menuItem1a, menuItem1b);
+        menuItem1a.setOnAction(actionEvent -> {
+            try {
+                readingRoom2.automaticSystem(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem1b.setOnAction(actionEvent -> {
+            try {
+                readingRoom2.manualSystem(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        MenuItem menuItem2a = new MenuItem("Add book");
+        MenuItem menuItem2b = new MenuItem("Book Info");
+        MenuItem menuItem2c = new MenuItem("Remove Book");
+        menuItem2a.setOnAction(actionEvent -> {
+            try {
+                addBook.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem2b.setOnAction(actionEvent -> {
+            try {
+                bookInformationShow.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem2c.setOnAction(actionEvent -> {
+            try {
+                removeBook.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        menu2.getItems().addAll(menuItem2a, menuItem2b, menuItem2c);
+
+        MenuItem menuItem3a = new MenuItem("Cross Validation");
+        MenuItem menuItem3b = new MenuItem("Seven Number Analysis");
+        MenuItem menuItem3c = new MenuItem("Data Optimization");
+        menuItem3a.setOnAction(actionEvent -> {
+            try {
+                crossValidation2.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem3b.setOnAction(actionEvent -> {
+            try {
+                sevenValueStatistics1.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem3c.setOnAction(actionEvent -> {
+            //     writerCollection.writerCollectionMLRMethods();
+        });
+
+        menu3.getItems().addAll(menuItem3a, menuItem3b, menuItem3c);
+
+        MenuItem menuItem4a = new MenuItem("Multi-variable Regression");
+        MenuItem menuItem4b = new MenuItem("Analytic Hierarchy Process");
+        MenuItem menuItem4c = new MenuItem("Page Rank Algorithm");
+        menu4.getItems().addAll(menuItem4a, menuItem4b, menuItem4c);
+        menuItem4a.setOnAction(actionEvent -> {
+            try {
+                fourVariableRegression1.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem4b.setOnAction(actionEvent -> {
+            try {
+                analyticHierarchyAlgorithm1.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        menuItem4c.setOnAction(actionEvent -> {
+            try {
+                pageRankAlgorithm1.start(primaryStage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        MenuItem readingRoom1 = new MenuItem("Reading Room");
+        readingRoom1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ReadingRoom readingRoom = new ReadingRoom();
+                try {
+                    {
+                        readingRoom.start(primaryStage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        MenuItem libraryDesk1 = new MenuItem("Library Desk");
+        libraryDesk1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                LibraryDesk libraryDesk = new LibraryDesk();
+                try {
+                    {
+                        libraryDesk.start(primaryStage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        MenuItem processVisualization = new MenuItem("Process Visualization");
+
+        processVisualization.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ProcessImplementation processImplementation = new ProcessImplementation();
+                {
+                    try {
+                        processImplementation.start(primaryStage);
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
+                }
+            }
+        });
+        MenuItem multivariableLinearRegression = new MenuItem("Multi-variable Linear Regression");
+        multivariableLinearRegression.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                FourVariableRegression fourVariableRegression = new FourVariableRegression();
+                try {
+                    {
+                        fourVariableRegression.start(primaryStage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        MenuItem analyticHierarchyProcess = new MenuItem("AnalyticHierarchy Process");
+        analyticHierarchyProcess.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                AnalyticHierarchyAlgorithm analyticHierarchyAlgorithm = new AnalyticHierarchyAlgorithm();
+                try {
+                    {
+                        analyticHierarchyAlgorithm.start(primaryStage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        MenuItem pageRankAlgorithm = new MenuItem("PageRank Algorithm");
+        pageRankAlgorithm.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                PageRankAlgorithm pageRankAlgorithm = new PageRankAlgorithm();
+                try {
+                    {
+                        pageRankAlgorithm.start(primaryStage);
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        MenuItem crossValidationProcess = new MenuItem("Cross ValidationProcess");
+        crossValidationProcess.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                CrossValidation crossValidation = new CrossValidation();
+                {
+                    try {
+                        crossValidation.start(primaryStage);
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
+                }
+            }
+        });
+        MenuItem trainingSetView = new MenuItem("Training Set View");
+        trainingSetView.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                TrainingSet trainingSet = new TrainingSet();
+                {
+                    trainingSet.start(primaryStage);
+                }
+            }
+        });
+
+        MenuItem testingSetView = new MenuItem("Testing Set View");
+        testingSetView.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                TestingSet testingSet = new TestingSet();
+                {
+                    testingSet.start(primaryStage);
+                }
+            }
+        });
+
+        MenuItem bookInformationView = new MenuItem("Book Information");
+
+        bookInformationView.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                BookInformationShow bookInformationShow = new BookInformationShow();
+                {
+                    try {
+                        bookInformationShow.start(primaryStage);
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        MenuItem systemAnalysis = new MenuItem("System Analysis");
+        systemAnalysis.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                ProcessAnalysis processAnalysis = new ProcessAnalysis();
+                {
+                    try {
+                        processAnalysis.start(primaryStage);
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        menu5.getItems().addAll(readingRoom1, libraryDesk1,
+                processVisualization, multivariableLinearRegression,
+                analyticHierarchyProcess, pageRankAlgorithm,
+                crossValidationProcess, trainingSetView,
+                testingSetView, bookInformationView, systemAnalysis);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menu1, menu2, menu3, menu4, menu5);
+        menuBar.setStyle("-fx-text-fill: #000000;");
+        menuBar.setStyle("-fx-background-color: #ffffff;");
+        menuBar.setStyle("-fx-padding: 1 5 1 5;");
+        menuBar.setStyle(" -fx-spacing: 47;");
+        menuBar.prefHeight(32);
+        group.getChildren().add(menuBar);
+        Scene scene1 = new Scene(group, 1500, 950);
+        menuBar.prefWidthProperty().bind(scene1.widthProperty());
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Books Statistics");
         primaryStage.setFullScreen(true);
