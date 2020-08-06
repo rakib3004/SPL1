bd15a52daa12756e2024738b332d45547fdaf04b
rakib3004 <bsse1129@iit.du.ac.bd>
2020-08-06 17:49:01 +0600

    remove old Button UI in TypesOfBook.java

--- src/BookDataBaseFX/TypesOfBook.java
+++ src/BookDataBaseFX/TypesOfBook.java
@@ -31,53 +31,16 @@ InfoPieChart infoPieChart = new InfoPieChart();
 
     @Override
     public void start(Stage primaryStage) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
+        dateTimeWriter.dateTimeWriterMethods(className);
+        Font font = new Font(14);
+        Font font1 = new Font(47);
+        Font font2 = new Font(26);
 
-/*
-
-
-        Font font = Font.font("Verdana", FontWeight.EXTRA_BOLD, 25);
-        Font font2 = Font.font("Verdana", FontWeight.BOLD, 8);
-        Font font1 = Font.font("Times New Roman", FontPosture.ITALIC, 18);
-
-
-        //  Button Start = new Button("Start");
-        Button back = new Button("Back");
-        Button exit = new Button("Exit");
         Button barChart = new Button("Bar Chart");
         Button pieChart = new Button("Pie Chart");
-
-
-        //  Start.setTranslateX(500);
-        //  Start.setTranslateY(400);
-
-
-        back.setTranslateX(0);
-        back.setTranslateY(650);
-        exit.setTranslateX(1100);
-        exit.setTranslateY(650);
-
-        barChart.setTranslateX(500);
-        barChart.setTranslateY(200);
-
-        pieChart.setTranslateX(500);
-        pieChart.setTranslateY(300);
-
-
-        back.setOnAction(actionEvent -> {
-            BookInformationShow bookInformationShow = new BookInformationShow();
-            try {
-                bookInformationShow.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-
-        });
-
-        exit.setOnAction(actionEvent -> {
-            System.exit(0);
-
-
-        });
+        Button analyticChart = new Button("Analytic Chart");
         barChart.setOnAction(actionEvent -> {
             try {
                 infoBarChart.startTypeBook(primaryStage);
@@ -97,60 +60,7 @@ InfoPieChart infoPieChart = new InfoPieChart();
             }
 
         });
-
-
-
-        //    setStyle(Start);
-        setStyle(exit);
-        setStyle(back);
-
-        setStyle(barChart);
-        setStyle(pieChart);
-
-        //  Start.setPrefSize(200, 80);
-        back.setPrefSize(200, 80);
-        exit.setPrefSize(200, 80);
-
-        barChart.setPrefSize(200, 80);
-
-        pieChart.setPrefSize(200, 80);
-
-
-
-
-
-        Image image = new Image("Images"+ File.separator +"libraryBackground5.jpg");
-        Canvas canvas = new Canvas(1500,950);
-        Group group = new Group();
-        group.getChildren().addAll(canvas,exit,back,barChart,pieChart);
-*/
-        String className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter = new DateTimeWriter();
-        dateTimeWriter.dateTimeWriterMethods(className);
-        Font font = new Font(14);
-        Font font1 = new Font(47);
-        Font font2 = new Font(26);
-
-        Button scatterChart = new Button("Scatter Chart");
-        Button lineChart = new Button("Line Chart");
-        Button stackedAreaChart = new Button("Stacked Area Chart");
-        scatterChart.setOnAction(actionEvent -> {
-            TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
-            try {
-                trainingTestingComparison.startScatterChart(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        lineChart.setOnAction(actionEvent -> {
-            TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
-            try {
-                trainingTestingComparison.startLineChart(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        stackedAreaChart.setOnAction(actionEvent -> {
+        analyticChart.setOnAction(actionEvent -> {
             TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
             try {
                 trainingTestingComparison.startStackedAreaChart(primaryStage);
@@ -158,23 +68,23 @@ InfoPieChart infoPieChart = new InfoPieChart();
                 exception.printStackTrace();
             }
         });
-        scatterChart.setPrefSize(410, 230);
-        lineChart.setPrefSize(410, 230);
-        stackedAreaChart.setPrefSize(410, 230);
+        barChart.setPrefSize(410, 230);
+        pieChart.setPrefSize(410, 230);
+        analyticChart.setPrefSize(410, 230);
 
-        scatterChart.setFont(font2);
-        lineChart.setFont(font2);
-        stackedAreaChart.setFont(font2);
-        scatterChart.setTranslateX(30);
-        scatterChart.setTranslateY(35);
-        lineChart.setTranslateX(30);
-        lineChart.setTranslateY(270);
-        stackedAreaChart.setTranslateX(30);
-        stackedAreaChart.setTranslateY(505);
+        barChart.setFont(font2);
+        pieChart.setFont(font2);
+        analyticChart.setFont(font2);
+        barChart.setTranslateX(30);
+        barChart.setTranslateY(35);
+        pieChart.setTranslateX(30);
+        pieChart.setTranslateY(270);
+        analyticChart.setTranslateX(30);
+        analyticChart.setTranslateY(505);
 
-        scatterChart.setContentDisplay(ContentDisplay.TOP);
-        lineChart.setContentDisplay(ContentDisplay.TOP);
-        stackedAreaChart.setContentDisplay(ContentDisplay.TOP);
+        barChart.setContentDisplay(ContentDisplay.TOP);
+        pieChart.setContentDisplay(ContentDisplay.TOP);
+        analyticChart.setContentDisplay(ContentDisplay.TOP);
         FileInputStream fileInputStream1 = null;
         try {
             fileInputStream1 = new FileInputStream(
@@ -183,7 +93,7 @@ InfoPieChart infoPieChart = new InfoPieChart();
             e.printStackTrace();
         }
         Image image1 = new Image(fileInputStream1);
-        scatterChart.setGraphic(new ImageView(image1));
+        barChart.setGraphic(new ImageView(image1));
 
         FileInputStream fileInputStream2 = null;
         try {
@@ -193,7 +103,7 @@ InfoPieChart infoPieChart = new InfoPieChart();
             e.printStackTrace();
         }
         Image image2 = new Image(fileInputStream2);
-        lineChart.setGraphic(new ImageView(image2));
+        pieChart.setGraphic(new ImageView(image2));
 
         FileInputStream fileInputStream3 = null;
         try {
@@ -203,7 +113,7 @@ InfoPieChart infoPieChart = new InfoPieChart();
             e.printStackTrace();
         }
         Image image3 = new Image(fileInputStream3);
-        stackedAreaChart.setGraphic(new ImageView(image3));
+        analyticChart.setGraphic(new ImageView(image3));
 
         Button exit = new Button("Exit");
         exit.setTranslateX(1200);
@@ -270,12 +180,13 @@ InfoPieChart infoPieChart = new InfoPieChart();
         Image image6 = new Image(fileInputStream6);
         back.setGraphic(new ImageView(image6));
         back.setOnAction(actionEvent -> {
-            CrossValidation crossValidation = new CrossValidation();
+            BookInformationShow bookInformationShow = new BookInformationShow();
             try {
-                crossValidation.finish(primaryStage);
+                bookInformationShow.start(primaryStage);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
+
         });
         Image background = new Image("Images" + File.separator + "framework.jpg");
         Canvas canvas = new Canvas(850, 425);
@@ -284,8 +195,8 @@ InfoPieChart infoPieChart = new InfoPieChart();
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         graphicsContext.drawImage(background, 0, 0);
         Group group = new Group();
-        group.getChildren().addAll(canvas, scatterChart,
-                lineChart, exit, back, home, text, stackedAreaChart);
+        group.getChildren().addAll(canvas, barChart,
+                pieChart, exit, back, home, text, analyticChart);
         Scene scene1 = new Scene(group,1500,950);
 
 
