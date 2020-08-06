39605a705714c76b57a3e5d6fe2f275cb4b6c2e3
rakib3004 <bsse1129@iit.du.ac.bd>
2020-08-06 17:46:12 +0600

    add new Button UI in TypesOfBook.java

--- src/BookDataBaseFX/TypesOfBook.java
+++ src/BookDataBaseFX/TypesOfBook.java
@@ -1,6 +1,10 @@
 package BookDataBaseFX;
 
+import CrossValidationProcess.CrossValidation;
+import CrossValidationProcess.TrainingTestingComparison;
+import FilePackage.DateTimeWriter;
 import InfoDisplay.BookInformationShow;
+import JavFX.Main;
 import VisualRepresentation.InfoBarChart;
 import VisualRepresentation.InfoPieChart;
 import javafx.application.Application;
@@ -9,13 +13,16 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.image.Image;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class TypesOfBook extends Application {
 
@@ -25,6 +32,7 @@ InfoPieChart infoPieChart = new InfoPieChart();
     @Override
     public void start(Stage primaryStage) {
 
+/*
 
 
         Font font = Font.font("Verdana", FontWeight.EXTRA_BOLD, 25);
@@ -115,12 +123,169 @@ InfoPieChart infoPieChart = new InfoPieChart();
         Canvas canvas = new Canvas(1500,950);
         Group group = new Group();
         group.getChildren().addAll(canvas,exit,back,barChart,pieChart);
+*/
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
+        dateTimeWriter.dateTimeWriterMethods(className);
+        Font font = new Font(14);
+        Font font1 = new Font(47);
+        Font font2 = new Font(26);
 
+        Button scatterChart = new Button("Scatter Chart");
+        Button lineChart = new Button("Line Chart");
+        Button stackedAreaChart = new Button("Stacked Area Chart");
+        scatterChart.setOnAction(actionEvent -> {
+            TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
+            try {
+                trainingTestingComparison.startScatterChart(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        lineChart.setOnAction(actionEvent -> {
+            TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
+            try {
+                trainingTestingComparison.startLineChart(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        stackedAreaChart.setOnAction(actionEvent -> {
+            TrainingTestingComparison trainingTestingComparison = new TrainingTestingComparison();
+            try {
+                trainingTestingComparison.startStackedAreaChart(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        scatterChart.setPrefSize(410, 230);
+        lineChart.setPrefSize(410, 230);
+        stackedAreaChart.setPrefSize(410, 230);
 
+        scatterChart.setFont(font2);
+        lineChart.setFont(font2);
+        stackedAreaChart.setFont(font2);
+        scatterChart.setTranslateX(30);
+        scatterChart.setTranslateY(35);
+        lineChart.setTranslateX(30);
+        lineChart.setTranslateY(270);
+        stackedAreaChart.setTranslateX(30);
+        stackedAreaChart.setTranslateY(505);
+
+        scatterChart.setContentDisplay(ContentDisplay.TOP);
+        lineChart.setContentDisplay(ContentDisplay.TOP);
+        stackedAreaChart.setContentDisplay(ContentDisplay.TOP);
+        FileInputStream fileInputStream1 = null;
+        try {
+            fileInputStream1 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "scatter.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image1 = new Image(fileInputStream1);
+        scatterChart.setGraphic(new ImageView(image1));
+
+        FileInputStream fileInputStream2 = null;
+        try {
+            fileInputStream2 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "line.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image2 = new Image(fileInputStream2);
+        lineChart.setGraphic(new ImageView(image2));
+
+        FileInputStream fileInputStream3 = null;
+        try {
+            fileInputStream3 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "area.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image3 = new Image(fileInputStream3);
+        stackedAreaChart.setGraphic(new ImageView(image3));
+
+        Button exit = new Button("Exit");
+        exit.setTranslateX(1200);
+        exit.setTranslateY(700);
+        exit.setOnAction(actionEvent -> {
+            System.exit(0);
+        });
+        FileInputStream fileInputStream4 = null;
+        try {
+            fileInputStream4 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "exit.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image4 = new Image(fileInputStream4);
+        exit.setGraphic(new ImageView(image4));
+        exit.setPrefSize(120, 20);
+        exit.setContentDisplay(ContentDisplay.LEFT);
+        exit.setFont(font);
+        Text text = new Text("Training Testing Comparison");
+        text.setTranslateX(600);
+        text.setTranslateY(500);
+        text.setFont(font1);
+        text.setFill(Color.BLACK);
+        text.setTextAlignment(TextAlignment.LEFT);
+        text.setStyle("-fx-font-weight: bold;");
+
+        Button home = new Button("Home");
+        home.setTranslateX(470);
+        home.setTranslateY(520);
+        home.setPrefSize(375, 30);
+        home.setFont(font2);
+        home.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream5 = null;
+        try {
+            fileInputStream5 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "home.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image5 = new Image(fileInputStream5);
+        home.setGraphic(new ImageView(image5));
+        home.setOnAction(actionEvent -> {
+            Main main = new Main();
+            try {
+                main.start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Button back = new Button("Back");
+        back.setTranslateX(950);
+        back.setTranslateY(520);
+        back.setPrefSize(375, 30);
+        back.setFont(font2);
+        back.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream6 = null;
+        try {
+            fileInputStream6 = new FileInputStream(
+                    "src" + File.separator + "Images" + File.separator + "back.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image6 = new Image(fileInputStream6);
+        back.setGraphic(new ImageView(image6));
+        back.setOnAction(actionEvent -> {
+            CrossValidation crossValidation = new CrossValidation();
+            try {
+                crossValidation.finish(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Image background = new Image("Images" + File.separator + "framework.jpg");
+        Canvas canvas = new Canvas(850, 425);
+        canvas.setTranslateX(470);
+        canvas.setTranslateY(35);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(image,0,0);
-
-
+        graphicsContext.drawImage(background, 0, 0);
+        Group group = new Group();
+        group.getChildren().addAll(canvas, scatterChart,
+                lineChart, exit, back, home, text, stackedAreaChart);
         Scene scene1 = new Scene(group,1500,950);
 
 
