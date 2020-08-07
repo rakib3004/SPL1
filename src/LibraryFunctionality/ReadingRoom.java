3caac215095cd4aa38b3a577a7c6f743c1a87401
rakib3004 <bsse1129@iit.du.ac.bd>
2020-08-07 11:23:58 +0600

    add new pic-> photo.png

--- src/LibraryFunctionality/ReadingRoom.java
+++ src/LibraryFunctionality/ReadingRoom.java
@@ -896,7 +896,7 @@ signUp.setTextFill(Color.BLACK);
         exit.setTranslateX(1100);
         exit.setTranslateY(650);
 
-        Image image = new Image("Images"+ File.separator +"pht.jpg");
+        Image image = new Image("Images"+ File.separator +"power.png");
         Canvas canvas = new Canvas(1500,950);
         Group group = new Group();
         group.getChildren().addAll(canvas,exit,back);
