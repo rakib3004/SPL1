8b7c02abd2cc3a1c83eb7c2f7c4a800903ad1f86
pc <bsse1129@iit.du.ac.bd>
2020-09-27 11:13:25 +0600

    add 2 label in PRA_Chart_View.class

--- src/TableViewPackage/PRA_Chart_View.java
+++ src/TableViewPackage/PRA_Chart_View.java
@@ -18,8 +18,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -78,10 +81,35 @@ public class PRA_Chart_View extends Application {
 
         series1.setName("Book Weight Show");
         int positionIndicator = 0;
+        double getWeightAverage=0.0;
+
         for (iterator = 0; iterator < numberOfBooks; iterator++) {
             positionIndicator++;
             series1.getData().add(new XYChart.Data(String.valueOf(positionIndicator), priorityData[iterator].getPRAweight()));
+
+            getWeightAverage = getWeightAverage+ priorityData[iterator].getPRAweight();
         }
+
+        getWeightAverage = getWeightAverage/numberOfBooks;
+
+        double standardDeviation=0.0;
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            standardDeviation = standardDeviation+ Math.pow(priorityData[iterator].getPRAweight()-getWeightAverage,2);
+        }
+        standardDeviation/=numberOfBooks;
+        standardDeviation=Math.sqrt(standardDeviation);
+        Label labelMean = new Label("Mean : "+getWeightAverage);
+        Label labelDeviation = new Label("Standard Deviation : "+standardDeviation);
+        Font fontModel = Font.font(Font.getFontNames().get(0), FontWeight.BOLD,25);
+        labelMean.setFont(fontModel);
+        labelDeviation.setFont(fontModel);
+        labelMean.setTranslateX(500);
+        labelMean.setTranslateY(350);
+        labelMean.setPrefSize(300,50);
+        labelDeviation.setTranslateX(500);
+        labelDeviation.setTranslateY(400);
+        labelDeviation.setPrefSize(300,50);
+
         lineChart.getData().add(series1);
         lineChart.setTranslateX(10);
         lineChart.setTranslateY(25);
@@ -127,7 +155,7 @@ public class PRA_Chart_View extends Application {
                 //  Image image = new Image("libraryBackground9.jpg");
         Canvas canvas = new Canvas(1500, 950);
         Group group = new Group();
-        group.getChildren().addAll(canvas, lineChart, exit, back);
+        group.getChildren().addAll(canvas, lineChart, exit, back,labelMean,labelDeviation);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         Scene scene1 = new Scene(group, 1500, 950);
