0d9c358136643118a2826735d114c722817a1fbb
pc <bsse1129@iit.du.ac.bd>
2020-09-29 21:18:38 +0600

    add new ButtonDesign.java

---
+++ src/Layout/ButtonDesign.java
@@ -0,0 +1,22 @@
+package Layout;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class ButtonDesign extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+    public Button systemDesign(Button button,int w, int h, int x, int y, int fontSize){
+
+        return  button;
+    }
+
+    }
