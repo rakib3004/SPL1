1a0f8e44c6419cc3875d0634bf5333cac021b706
pc <bsse1129@iit.du.ac.bd>
2020-09-29 23:23:32 +0600

    add new  font in ButtonDesign.java

--- src/Layout/ButtonDesign.java
+++ src/Layout/ButtonDesign.java
@@ -2,7 +2,11 @@ package Layout;
 
 import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class ButtonDesign extends Application {
 
@@ -14,8 +18,12 @@ public class ButtonDesign extends Application {
     public void start(Stage primaryStage) {
 
     }
-    public Button systemDesign(Button button,int w, int h, int x, int y, int fontSize){
-
+    public Button systemDesign(Button button,int w, int h, int x, int y, double fontSize){
+      button.setPrefSize(w,h);
+      button.setTranslateX(x);
+      button.setTranslateY(y);
+        Font font = Font.font(Font.getFontNames().get(0),
+                FontWeight.BOLD,fontSize);
         return  button;
     }
 
