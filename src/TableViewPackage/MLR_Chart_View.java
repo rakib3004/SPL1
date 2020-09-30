1e6ea5c9c83aca53697859fe4bc088c35f3f676a
pc <bsse1129@iit.du.ac.bd>
2020-09-30 16:08:02 +0600

    remove extra features in MLR_Chart_View.java

--- src/TableViewPackage/MLR_Chart_View.java
+++ src/TableViewPackage/MLR_Chart_View.java
@@ -142,7 +142,7 @@ MultipleLinearRegression multipleLinearRegression = new MultipleLinearRegression
         exit.setPrefSize(120, 20);
         exit.setContentDisplay(ContentDisplay.LEFT);
         exit.setFont(font);
-        Text text = new Text("Analytic Hierarchy Algorithm");
+        Text text = new Text("Multiple Linear Regression");
         text.setTranslateX(520);
         text.setTranslateY(500);
         text.setFont(font1);
@@ -315,7 +315,7 @@ MultipleLinearRegression multipleLinearRegression = new MultipleLinearRegression
         exit.setPrefSize(120, 20);
         exit.setContentDisplay(ContentDisplay.LEFT);
         exit.setFont(font);
-        Text text = new Text("Analytic Hierarchy Algorithm");
+        Text text = new Text("Multiple Linear Regression");
         text.setTranslateX(520);
         text.setTranslateY(500);
         text.setFont(font1);
@@ -597,7 +597,7 @@ labelDeviation.setPrefSize(300,50);
         exit.setPrefSize(120, 20);
         exit.setContentDisplay(ContentDisplay.LEFT);
         exit.setFont(font);
-        Text text = new Text("Analytic Hierarchy Algorithm");
+        Text text = new Text("Multiple Linear Regression");
         text.setTranslateX(520);
         text.setTranslateY(500);
         text.setFont(font1);
@@ -682,72 +682,6 @@ labelDeviation.setPrefSize(300,50);
         lineChart.setTranslateX(0);
         lineChart.setTranslateY(0);
         lineChart.setPrefSize(1350, 600);
-        // lineChart.setStyle("-fx-chart-plot-background-color: transperant;");
-        ContextMenu contextMenu = new ContextMenu();
-        MenuItem mlr_process = new MenuItem("MLR Process");
-        MenuItem ahp_process = new MenuItem("AHP Process");
-        MenuItem pra_process = new MenuItem("PRA Process");
-
-        mlr_process.setOnAction((event) -> {
-            try {
-                MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
-                mlr_chart_view.start(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        ahp_process.setOnAction((event) -> {
-            AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-            try {
-                ahp_chart_view.start(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        pra_process.setOnAction((event) -> {
-            PRA_Chart_View pra_chart_view = new PRA_Chart_View();
-            try {
-                pra_chart_view.start(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
-        lineChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            @Override
-            public void handle(ContextMenuEvent event) {
-
-                contextMenu.show(lineChart, event.getScreenX(), event.getScreenY());
-            }
-        });
-
-        Button scatterChartView = new Button("ScatterChart View");
-        Button stackedAreaChartView = new Button("StackedArea View");
-
-        scatterChartView.setOnAction(actionEvent -> {
-            try {
-                startScatterChart(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        stackedAreaChartView.setOnAction(actionEvent -> {
-            try {
-                startStackedAreaChart(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        scatterChartView.setTranslateX(1000);
-        scatterChartView.setTranslateY(600);
-        stackedAreaChartView.setTranslateX(1150);
-        stackedAreaChartView.setTranslateY(600);
-        scatterChartView.setPrefSize(145, 90);
-        stackedAreaChartView.setPrefSize(145, 90);
-        Canvas canvas = new Canvas(1500, 950);
-        Group group = new Group();
-        group.getChildren().addAll(canvas, lineChart, stackedAreaChartView, scatterChartView,exit,back);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         Scene scene1 = new Scene(group, 1500, 950);
         scene1.getStylesheets().add("LineChart.css");
@@ -761,30 +695,162 @@ labelDeviation.setPrefSize(300,50);
         String  className = this.getClass().getSimpleName();
         DateTimeWriter dateTimeWriter =  new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
-        Button back = new Button("Back");
-        Button exit = new Button("Exit");
-        back.setTranslateX(0);
-        back.setTranslateY(650);
-        exit.setTranslateX(1100);
-        exit.setTranslateY(650);
+        Font font = new Font(19);
+        Font font1 = new Font(47);
+        Font font2 = new Font( 26);
 
-        back.setOnAction(actionEvent -> {
-            FourVariableRegression fourVariableRegression = new FourVariableRegression();
+        Button scatter_chart = new Button("Scatter Chart");
+        Button line_chart = new Button("Line Chart");
+        Button stacked_area_chart = new Button("Stacked Area Chart");
+
+        scatter_chart.setPrefSize(410,230);
+        line_chart.setPrefSize(410,230);
+        stacked_area_chart.setPrefSize(410,230);
+
+        scatter_chart.setFont(font2);
+        line_chart.setFont(font2);
+        stacked_area_chart.setFont(font2);
+        scatter_chart.setTranslateX(30);
+        scatter_chart.setTranslateY(35);
+        line_chart.setTranslateX(30);
+        line_chart.setTranslateY(270);
+        stacked_area_chart.setTranslateX(30);
+        stacked_area_chart.setTranslateY(505);
+
+        scatter_chart.setContentDisplay(ContentDisplay.TOP);
+        line_chart.setContentDisplay(ContentDisplay.TOP);
+        stacked_area_chart.setContentDisplay(ContentDisplay.TOP);
+        FileInputStream fileInputStream1 = null;
+        try {
+            fileInputStream1 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"scatter.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image1 = new Image(fileInputStream1);
+        scatter_chart.setGraphic(new ImageView(image1));
+
+        FileInputStream fileInputStream2 = null;
+        try {
+            fileInputStream2 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"line.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image2 = new Image(fileInputStream2);
+        line_chart.setGraphic(new ImageView(image2));
+
+        FileInputStream fileInputStream3 = null;
+        try {
+            fileInputStream3 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"area.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image3 = new Image(fileInputStream3);
+        stacked_area_chart.setGraphic(new ImageView(image3));
+
+        scatter_chart.setOnAction(actionEvent -> {
             try {
-                fourVariableRegression.start(primaryStage);
+                startScatterChart(primaryStage);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
+
+        stacked_area_chart.setOnAction(actionEvent -> {
+            try {
+                startStackedAreaChart(primaryStage);
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        line_chart.setOnAction(actionEvent -> {
+            try {
+                startLineChart(primaryStage);
+
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+        Button exit = new Button("Exit");
+        exit.setTranslateX(1200);
+        exit.setTranslateY(700);
         exit.setOnAction(actionEvent -> {
             System.exit(0);
         });
+        FileInputStream fileInputStream4 = null;
+        try {
+            fileInputStream4 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"exit.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image4 = new Image(fileInputStream4);
+        exit.setGraphic(new ImageView(image4));
+        exit.setPrefSize(120, 20);
+        exit.setContentDisplay(ContentDisplay.LEFT);
+        exit.setFont(font);
+        Text text = new Text("Multiple Linear Regression");
+        text.setTranslateX(520);
+        text.setTranslateY(500);
+        text.setFont(font1);
+        text.setFill(Color.BLACK);
+        text.setTextAlignment(TextAlignment.LEFT);
+        text.setStyle("-fx-font-weight: bold;");
 
-        setStyle(exit);
-        setStyle(back);
-        back.setPrefSize(200, 80);
-        exit.setPrefSize(200, 80);
+        Button home = new Button("Home");
+        home.setTranslateX(470);
+        home.setTranslateY(520);
+        home.setPrefSize(375,30);
+        home.setFont(font2);
+        home.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream5 = null;
+        try {
+            fileInputStream5 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"home.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image5 = new Image(fileInputStream5);
+        home.setGraphic(new ImageView(image5));
+        home.setOnAction(actionEvent -> {
+            Main main = new Main();
+            try {
+                main.start(primaryStage);
+            }
+            catch (Exception exception){
+                exception.printStackTrace();
+            }
+        });
+        Button back = new Button("Back");
+        back.setTranslateX(950);
+        back.setTranslateY(520);
+        back.setPrefSize(375,30);
+        back.setFont(font2);
+        back.setContentDisplay(ContentDisplay.LEFT);
+        FileInputStream fileInputStream6 = null;
+        try {
+            fileInputStream6 = new FileInputStream(
+                    "src"+ File.separator +"Images"+ File.separator +"back.png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Image image6 = new Image(fileInputStream6);
+        back.setGraphic(new ImageView(image6));
+        back.setOnAction(actionEvent -> {
+            try {
+                start(primaryStage);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
+        });
 
+        Group group = new Group();
+        group.getChildren().addAll(scatter_chart,
+                line_chart,
+                stacked_area_chart,exit,home,text,back);
         priorityData = processing.fileReaderMethods();
         numberOfBooks = bookNumber.bookNumberFindingMethods();
         priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
@@ -811,72 +877,6 @@ labelDeviation.setPrefSize(300,50);
         stackedAreaChart.setTranslateY(25);
         stackedAreaChart.setPrefSize(1350, 700);
 
-        ContextMenu contextMenu = new ContextMenu();
-        MenuItem mlr_process = new MenuItem("MLR Process");
-        MenuItem ahp_process = new MenuItem("AHP Process");
-        MenuItem pra_process = new MenuItem("PRA Process");
-
-        mlr_process.setOnAction((event) -> {
-            try {
-                MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
-                mlr_chart_view.startStackedAreaChart(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        ahp_process.setOnAction((event) -> {
-            AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-            try {
-                ahp_chart_view.startStackedAreaChart(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        pra_process.setOnAction((event) -> {
-            PRA_Chart_View pra_chart_view = new PRA_Chart_View();
-            try {
-                pra_chart_view.startStackedAreaChart(primaryStage);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
-        stackedAreaChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            @Override
-            public void handle(ContextMenuEvent event) {
-                contextMenu.show(stackedAreaChart, event.getScreenX(), event.getScreenY());
-            }
-        });
-        Button scatterChartView = new Button("ScatterChart View");
-        Button lineChartView = new Button("LineChart View");
-        scatterChartView.setTranslateX(30);
-        scatterChartView.setTranslateY(0);
-        lineChartView.setTranslateX(1170);
-        lineChartView.setTranslateY(0);
-
-        scatterChartView.setOnAction(actionEvent -> {
-            try {
-                startScatterChart(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        lineChartView.setOnAction(actionEvent -> {
-            try {
-                start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        //  Image image = new Image("libraryBackground9.jpg");
-        Canvas canvas = new Canvas(1500, 950);
-        Group group = new Group();
-        group.getChildren().addAll(canvas, stackedAreaChart, exit, back
-        ,scatterChartView,lineChartView);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
         Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
