02caa6f70e8ef2b25f9ad8e500ad50ea0a4c8f81
pc <bsse1129@iit.du.ac.bd>
2020-09-30 12:46:05 +0600

    add stacked area chart new design in PRA_Chart_View.startStackedAreaChart

--- src/TableViewPackage/PRA_Chart_View.java
+++ src/TableViewPackage/PRA_Chart_View.java
@@ -406,9 +406,7 @@ public class PRA_Chart_View extends Application {
             series1.getData().add(new XYChart.Data(String.valueOf(positionIndicator), priorityData[iterator].getPRAweight()));
         }
 
-        scatterChart.getData().add(series1);
-        scatterChart = buttonDesign.systemLine(scatterChart,850,350,470,35);
-        group.getChildren().add(scatterChart);
+
 
       double getWeightAverage=0.0;
       for (iterator = 0; iterator < numberOfBooks; iterator++) {
@@ -427,6 +425,9 @@ public class PRA_Chart_View extends Application {
       labelDeviation = buttonDesign.systemLine(labelDeviation,750,410,25);
       group.getChildren().addAll(labelMean,labelDeviation);
 
+      scatterChart.getData().add(series1);
+      scatterChart = buttonDesign.systemLine(scatterChart,850,350,470,35);
+      group.getChildren().add(scatterChart);
       Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
@@ -653,29 +654,166 @@ public class PRA_Chart_View extends Application {
         DateTimeWriter dateTimeWriter =  new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
-        Button back = new Button("Back");
-        Button exit = new Button("Exit");
-        back.setTranslateX(0);
-        back.setTranslateY(650);
-        exit.setTranslateX(1100);
-        exit.setTranslateY(650);
-        back.setOnAction(actionEvent -> {
-            PageRankAlgorithm pageRankAlgorithm = new PageRankAlgorithm();
-            try {
-                pageRankAlgorithm.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-        exit.setOnAction(actionEvent -> {
-            System.exit(0);
-        });
-        setStyle(exit);
-        setStyle(back);
-        back.setPrefSize(200, 80);
-        exit.setPrefSize(200, 80);
 
-        priorityData = processing.fileReaderMethods();
+      Font font = new Font(19);
+      Font font1 = new Font(47);
+      Font font2 = new Font( 26);
+
+      Button scatter_chart = new Button("Scatter Chart");
+      Button line_chart = new Button("Line Chart");
+      Button stacked_area_chart = new Button("Stacked Area Chart");
+
+      scatter_chart.setPrefSize(410,230);
+      line_chart.setPrefSize(410,230);
+      stacked_area_chart.setPrefSize(410,230);
+
+      scatter_chart.setFont(font2);
+      line_chart.setFont(font2);
+      stacked_area_chart.setFont(font2);
+      scatter_chart.setTranslateX(30);
+      scatter_chart.setTranslateY(35);
+      line_chart.setTranslateX(30);
+      line_chart.setTranslateY(270);
+      stacked_area_chart.setTranslateX(30);
+      stacked_area_chart.setTranslateY(505);
+
+      scatter_chart.setContentDisplay(ContentDisplay.TOP);
+      line_chart.setContentDisplay(ContentDisplay.TOP);
+      stacked_area_chart.setContentDisplay(ContentDisplay.TOP);
+      FileInputStream fileInputStream1 = null;
+      try {
+          fileInputStream1 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"scatter.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image1 = new Image(fileInputStream1);
+      scatter_chart.setGraphic(new ImageView(image1));
+
+      FileInputStream fileInputStream2 = null;
+      try {
+          fileInputStream2 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"line.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image2 = new Image(fileInputStream2);
+      line_chart.setGraphic(new ImageView(image2));
+
+      FileInputStream fileInputStream3 = null;
+      try {
+          fileInputStream3 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"area.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image3 = new Image(fileInputStream3);
+      stacked_area_chart.setGraphic(new ImageView(image3));
+
+      scatter_chart.setOnAction(actionEvent -> {
+          try {
+              startScatterChart(primaryStage);
+          } catch (Exception exception) {
+              exception.printStackTrace();
+          }
+      });
+
+      stacked_area_chart.setOnAction(actionEvent -> {
+          try {
+              startStackedAreaChart(primaryStage);
+
+          } catch (Exception exception) {
+              exception.printStackTrace();
+          }
+      });
+      line_chart.setOnAction(actionEvent -> {
+          try {
+              startLineChart(primaryStage);
+
+          } catch (Exception exception) {
+              exception.printStackTrace();
+          }
+      });
+      Button exit = new Button("Exit");
+      exit.setTranslateX(1200);
+      exit.setTranslateY(700);
+      exit.setOnAction(actionEvent -> {
+          System.exit(0);
+      });
+      FileInputStream fileInputStream4 = null;
+      try {
+          fileInputStream4 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"exit.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image4 = new Image(fileInputStream4);
+      exit.setGraphic(new ImageView(image4));
+      exit.setPrefSize(120, 20);
+      exit.setContentDisplay(ContentDisplay.LEFT);
+      exit.setFont(font);
+      Text text = new Text("Analytic Hierarchy Algorithm");
+      text.setTranslateX(520);
+      text.setTranslateY(500);
+      text.setFont(font1);
+      text.setFill(Color.BLACK);
+      text.setTextAlignment(TextAlignment.LEFT);
+      text.setStyle("-fx-font-weight: bold;");
+
+      Button home = new Button("Home");
+      home.setTranslateX(470);
+      home.setTranslateY(520);
+      home.setPrefSize(375,30);
+      home.setFont(font2);
+      home.setContentDisplay(ContentDisplay.LEFT);
+      FileInputStream fileInputStream5 = null;
+      try {
+          fileInputStream5 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"home.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image5 = new Image(fileInputStream5);
+      home.setGraphic(new ImageView(image5));
+      home.setOnAction(actionEvent -> {
+          Main main = new Main();
+          try {
+              main.start(primaryStage);
+          }
+          catch (Exception exception){
+              exception.printStackTrace();
+          }
+      });
+      Button back = new Button("Back");
+      back.setTranslateX(950);
+      back.setTranslateY(520);
+      back.setPrefSize(375,30);
+      back.setFont(font2);
+      back.setContentDisplay(ContentDisplay.LEFT);
+      FileInputStream fileInputStream6 = null;
+      try {
+          fileInputStream6 = new FileInputStream(
+                  "src"+ File.separator +"Images"+ File.separator +"back.png");
+      } catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      Image image6 = new Image(fileInputStream6);
+      back.setGraphic(new ImageView(image6));
+      back.setOnAction(actionEvent -> {
+          try {
+              start(primaryStage);
+          } catch (Exception exception) {
+              exception.printStackTrace();
+          }
+      });
+
+      Group group = new Group();
+      group.getChildren().addAll(scatter_chart,
+              line_chart,
+              stacked_area_chart,exit,home,text,back);
+
+
+      priorityData = processing.fileReaderMethods();
         numberOfBooks = bookNumber.bookNumberFindingMethods();
      //   priorityData = pageRankCalculation.pageRankCalculationMethods(priorityData,numberOfBooks);
       PageRankProcessData pageRankProcessData = new PageRankProcessData();
@@ -697,72 +835,33 @@ public class PRA_Chart_View extends Application {
             series1.getData().add(new XYChart.Data(String.valueOf(positionIndicator),
                     priorityData[iterator].getPRAweight()));
        }
+
+
         stackedAreaChart.getData().add(series1);
 
-        stackedAreaChart.setTranslateX(10);
-        stackedAreaChart.setTranslateY(25);
-        stackedAreaChart.setPrefSize(1350, 700);
-        ContextMenu contextMenu = new ContextMenu();
-        MenuItem mlr_process = new MenuItem("MLR Process");
-        MenuItem ahp_process = new MenuItem("AHP Process");
-        MenuItem pra_process = new MenuItem("PRA Process");
-        mlr_process.setOnAction((event) -> {
-          try {
-              MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
-              mlr_chart_view.startStackedAreaChart(primaryStage);
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-      });
-      ahp_process.setOnAction((event) -> {
-          AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-          try {
-              ahp_chart_view.startStackedAreaChart(primaryStage);
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-      });
-      pra_process.setOnAction((event) -> {
-          PRA_Chart_View pra_chart_view = new PRA_Chart_View();
-          try {
-              pra_chart_view.startStackedAreaChart(primaryStage);
-          } catch (IOException e) {
-              e.printStackTrace();
-          }
-      });
-        contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
-        stackedAreaChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            @Override
-            public void handle(ContextMenuEvent event) {
-                contextMenu.show(stackedAreaChart, event.getScreenX(), event.getScreenY());
-            }
-        });
+      stackedAreaChart = buttonDesign.systemLine(stackedAreaChart,850,350,470,35);
+      group.getChildren().add(stackedAreaChart);
 
-      Button scatterChartView = new Button("ScatterChart View");
-      Button lineChartView = new Button("LineChart View");
-      scatterChartView.setTranslateX(30);
-      scatterChartView.setTranslateY(0);
-      lineChartView.setTranslateX(1170);
-      lineChartView.setTranslateY(0);
-      scatterChartView.setOnAction(actionEvent -> {
-          try {
-              startScatterChart(primaryStage);
-          } catch (Exception exception) {
-              exception.printStackTrace();
-          }
-      });
-      lineChartView.setOnAction(actionEvent -> {
-          try {
-              start(primaryStage);
-          } catch (Exception exception) {
-              exception.printStackTrace();
-          }
-      });
-      Canvas canvas = new Canvas(1500, 950);
-      Group group = new Group();
-      group.getChildren().addAll(canvas, stackedAreaChart, exit, back
-              ,scatterChartView,lineChartView);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+      double getWeightAverage=0.0;
+      for (iterator = 0; iterator < numberOfBooks; iterator++) {
+          getWeightAverage = getWeightAverage+ priorityData[iterator].getPRAweight();
+      }
+      getWeightAverage = getWeightAverage/numberOfBooks;
+      double standardDeviation=0.0;
+      for (iterator = 0; iterator < numberOfBooks; iterator++) {
+          standardDeviation = standardDeviation+ Math.pow(priorityData[iterator].getPRAweight()-getWeightAverage,2);
+      }
+      standardDeviation/=numberOfBooks;
+      standardDeviation=Math.sqrt(standardDeviation);
+      Label labelMean = new Label("Mean : "+getWeightAverage);
+      Label labelDeviation = new Label("Standard Deviation : "+standardDeviation);
+      labelMean = buttonDesign.systemLine(labelMean,750,380,25);
+      labelDeviation = buttonDesign.systemLine(labelDeviation,750,410,25);
+      group.getChildren().addAll(labelMean,labelDeviation);
+
+
+
         Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
