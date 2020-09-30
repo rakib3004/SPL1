3ab80daacd90144a173f48c21499677e5940dc6d
pc <bsse1129@iit.du.ac.bd>
2020-09-30 15:51:11 +0600

    remove unused feature in AHP_Chart_View.java

--- src/TableViewPackage/AHP_Chart_View.java
+++ src/TableViewPackage/AHP_Chart_View.java
@@ -678,73 +678,6 @@ public class AHP_Chart_View extends Application {
      lineChart.setTranslateY(25);
      lineChart.setPrefSize(1350, 700);
 
-     ContextMenu contextMenu = new ContextMenu();
-     MenuItem mlr_process = new MenuItem("MLR Process");
-     MenuItem ahp_process = new MenuItem("AHP Process");
-     MenuItem pra_process = new MenuItem("PRA Process");
-
-     mlr_process.setOnAction((event) -> {
-         try {
-             MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
-             mlr_chart_view.start(primaryStage);
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-     });
-     ahp_process.setOnAction((event) -> {
-         AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-         try {
-             ahp_chart_view.start(primaryStage);
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-     });
-     pra_process.setOnAction((event) -> {
-         PRA_Chart_View pra_chart_view = new PRA_Chart_View();
-         try {
-             pra_chart_view.start(primaryStage);
-         } catch (IOException e) {
-             e.printStackTrace();
-         }
-     });
-     contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
-
-     lineChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-         @Override
-         public void handle(ContextMenuEvent event) {
-             contextMenu.show(lineChart, event.getScreenX(), event.getScreenY());
-         }
-     });
-
-     Button scatterChartView = new Button("ScatterChart View");
-     Button stackedAreaChartView = new Button("StackedAreaChart View");
-     scatterChartView.setTranslateX(30);
-     scatterChartView.setTranslateY(0);
-     stackedAreaChartView.setTranslateX(1170);
-     stackedAreaChartView.setTranslateY(0);
-     scatterChartView.setOnAction(actionEvent -> {
-         try {
-             startScatterChart(primaryStage);
-         } catch (Exception exception) {
-             exception.printStackTrace();
-         }
-     });
-
-     stackedAreaChartView.setOnAction(actionEvent -> {
-         try {
-             startStackedAreaChart(primaryStage);
-         } catch (Exception exception) {
-             exception.printStackTrace();
-         }
-     });
-     scatterChartView.setPrefSize(160, 40);
-     stackedAreaChartView.setPrefSize(160, 40);
-     Canvas canvas = new Canvas(1500, 950);
-     Group group = new Group();
-     group.getChildren().addAll(canvas, lineChart, exit, back
-             , stackedAreaChartView, scatterChartView,labelMean,labelDeviation);
-
-     GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
      Scene scene1 = new Scene(group, 1500, 950);
      primaryStage.setScene(scene1);
      primaryStage.setTitle("Recommendation Tool");
@@ -949,67 +882,7 @@ public void startStackedAreaChart(Stage primaryStage) throws IOException {
         stackedAreaChart.setTranslateX(10);
         stackedAreaChart.setTranslateY(25);
         stackedAreaChart.setPrefSize(1350, 700);
-        ContextMenu contextMenu = new ContextMenu();
-        MenuItem mlr_process = new MenuItem("MLR Process");
-        MenuItem ahp_process = new MenuItem("AHP Process");
-        MenuItem pra_process = new MenuItem("PRA Process");
 
-    mlr_process.setOnAction((event) -> {
-        try {
-            MLR_Chart_View mlr_chart_view = new MLR_Chart_View();
-            mlr_chart_view.startStackedAreaChart(primaryStage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    });
-    ahp_process.setOnAction((event) -> {
-        AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-        try {
-            ahp_chart_view.startStackedAreaChart(primaryStage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    });
-    pra_process.setOnAction((event) -> {
-        PRA_Chart_View pra_chart_view = new PRA_Chart_View();
-        try {
-            pra_chart_view.startStackedAreaChart(primaryStage);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    });
-    contextMenu.getItems().addAll(mlr_process, ahp_process, pra_process);
-        stackedAreaChart.setOnContextMenuRequested(new EventHandler<ContextMenuEvent>() {
-            @Override
-            public void handle(ContextMenuEvent event) {
-                contextMenu.show(stackedAreaChart, event.getScreenX(), event.getScreenY());
-            }
-        });
-    Button scatterChartView = new Button("ScatterChart View");
-    Button lineChartView = new Button("LineChart View");
-    scatterChartView.setTranslateX(30);
-    scatterChartView.setTranslateY(0);
-    lineChartView.setTranslateX(1170);
-    lineChartView.setTranslateY(0);
-    scatterChartView.setOnAction(actionEvent -> {
-        try {
-            startScatterChart(primaryStage);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    });
-    lineChartView.setOnAction(actionEvent -> {
-        try {
-            start(primaryStage);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-    });
-    Canvas canvas = new Canvas(1500, 950);
-    Group group = new Group();
-    group.getChildren().addAll(canvas, stackedAreaChart, exit, back
-            ,scatterChartView,lineChartView);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
        Scene scene1 = new Scene(group, 1500, 950);
 
         primaryStage.setScene(scene1);
