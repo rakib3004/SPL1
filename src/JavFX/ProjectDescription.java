ad85f934ed5924ee3cc7ef4e0ce1b0dd7cd2f115
pc <bsse1129@iit.du.ac.bd>
2020-10-09 17:24:29 +0600

    reduce code in ProjectDescription.java

--- src/JavFX/ProjectDescription.java
+++ src/JavFX/ProjectDescription.java
@@ -22,20 +22,20 @@ public class ProjectDescription extends Application {
         launch(args);
     }
 
-   @Override
-    public void start(Stage primaryStage,int index) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+
+    public void start(Stage primaryStage, int index) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Button back = new Button("Back");
         Button exit = new Button("Exit");
         back.setTranslateX(50);
-        back.setTranslateY(650);
+        back.setTranslateY(670);
         exit.setTranslateX(1100);
-        exit.setTranslateY(650);
+        exit.setTranslateY(670);
         back.setOnAction(actionEvent -> {
-   AuthorSystem authorSystem = new AuthorSystem();
+            AuthorSystem authorSystem = new AuthorSystem();
             try {
                 authorSystem.start(primaryStage);
             } catch (Exception exception) {
@@ -46,120 +46,64 @@ public class ProjectDescription extends Application {
         exit.setOnAction(actionEvent -> {
             System.exit(1129);
         });
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
+        Font font3 = Font.font(Font.getFontNames().get(0), FontWeight.BOLD, 30);
         back.setFont(font3);
         exit.setFont(font3);
         back.setPrefSize(200, 80);
         exit.setPrefSize(200, 80);
-        Image image = new Image("Images"+ File.separator +"v2.png");
+        String imageString = "v2.png";
 
-        if(index==1){
-
-    }
-else if(index==2){
-
-}
-else  if(index==3){
-
-}
-        Canvas canvas = new Canvas(1500,950);
-        Group group = new Group();
-        group.getChildren().addAll(canvas,exit,back);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(image,0,0);
-
-        Scene scene1 = new Scene(group,1500,950);
-        primaryStage.setScene(scene1);
-        primaryStage.setTitle("Recommendation Tool");
-        primaryStage.setFullScreen(true);
-        primaryStage.show();
-    }
- public void startCrossValidation(Stage primaryStage,int index) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
-        dateTimeWriter.dateTimeWriterMethods(className);
-
-        Button back = new Button("Back");
-        Button exit = new Button("Exit");
-        back.setTranslateX(50);
-        back.setTranslateY(650);
-        exit.setTranslateX(1100);
-        exit.setTranslateY(650);
-        back.setOnAction(actionEvent -> {
-   CrossValidation crossValidation = new CrossValidation();
-            try {
-                crossValidation.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }
-        });
-
-        exit.setOnAction(actionEvent -> {
-            System.exit(1129);
-        });
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
-        back.setFont(font3);
-        exit.setFont(font3);
-        back.setPrefSize(200, 80);
-        exit.setPrefSize(200, 80);
-
-        Image image = new Image("Images"+ File.separator +"v3.png");
-        Canvas canvas = new Canvas(1500,950);
-        Group group = new Group();
-        group.getChildren().addAll(canvas,exit,back);
-        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(image,0,0);
-
-        Scene scene1 = new Scene(group,1500,950);
-        primaryStage.setScene(scene1);
-        primaryStage.setTitle("Recommendation Tool");
-        primaryStage.setFullScreen(true);
-        primaryStage.show();
-    }
-
-public void startUserRules(Stage primaryStage,int index) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
-        dateTimeWriter.dateTimeWriterMethods(className);
-
-        Button back = new Button("Back");
-        Button exit = new Button("Exit");
-        back.setTranslateX(50);
-        back.setTranslateY(680);
-        exit.setTranslateX(1100);
-        exit.setTranslateY(680);
-        back.setOnAction(actionEvent -> {
-   ReadingRoom readingRoom = new ReadingRoom();
+        if (index == 1) {
+            imageString = "v2.png";
+            back.setOnAction(actionEvent -> {
+                AuthorSystem authorSystem = new AuthorSystem();
+                try {
+                    authorSystem.start(primaryStage);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            });
+        } else if (index == 2) {
+            imageString = "v3.png";
+            back.setOnAction(actionEvent -> {
+                CrossValidation crossValidation = new CrossValidation();
+                try {
+                    crossValidation.start(primaryStage);
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            });
+        } else if (index == 3) {
+            imageString = "v4.png";
+            ReadingRoom readingRoom = new ReadingRoom();
             try {
                 readingRoom.start(primaryStage);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
-        });
 
-        exit.setOnAction(actionEvent -> {
-            System.exit(1129);
-        });
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
-        back.setFont(font3);
-        exit.setFont(font3);
-        back.setPrefSize(200, 80);
-        exit.setPrefSize(200, 80);
+        }
+        Image image = new Image("Images" + File.separator + imageString);
 
-        Image image = new Image("Images"+ File.separator +"v4.png");
-        Canvas canvas = new Canvas(1500,950);
+        Canvas canvas = new Canvas(1500, 950);
         Group group = new Group();
-        group.getChildren().addAll(canvas,exit,back);
+        group.getChildren().addAll(canvas, exit, back);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(image,0,0);
+        graphicsContext.drawImage(image, 0, 0);
 
-        Scene scene1 = new Scene(group,1500,950);
+        Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
+
  /*
 
    Class tree/ Diagram ache naki kono
