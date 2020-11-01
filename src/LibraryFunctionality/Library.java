246c5f1a89b31d8ca00d37d97de1800731cc046a
pc <bsse1129@iit.du.ac.bd>
2020-11-02 00:20:13 +0600

    create new class  Library.java and LibraryUserShowing.java

---
+++ src/LibraryFunctionality/Library.java
@@ -0,0 +1,82 @@
+package LibraryFunctionality;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Library {
+    private SimpleStringProperty userID;
+    private SimpleStringProperty userName;
+    private SimpleStringProperty userInstitute;
+    private SimpleStringProperty userEducationLevel;
+    private SimpleStringProperty userClass;
+
+    public Library(SimpleStringProperty userID,
+                   SimpleStringProperty userName,
+                   SimpleStringProperty userInstitute,
+                   SimpleStringProperty userEducationLevel,
+                   SimpleStringProperty userClass) {
+        this.userID = userID;
+        this.userName = userName;
+        this.userInstitute = userInstitute;
+        this.userEducationLevel = userEducationLevel;
+        this.userClass = userClass;
+    }
+
+    public String getUserID() {
+        return userID.get();
+    }
+
+    public SimpleStringProperty userIDProperty() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID.set(userID);
+    }
+
+    public String getUserName() {
+        return userName.get();
+    }
+
+    public SimpleStringProperty userNameProperty() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName.set(userName);
+    }
+
+    public String getUserInstitute() {
+        return userInstitute.get();
+    }
+
+    public SimpleStringProperty userInstituteProperty() {
+        return userInstitute;
+    }
+
+    public void setUserInstitute(String userInstitute) {
+        this.userInstitute.set(userInstitute);
+    }
+
+    public String getUserEducationLevel() {
+        return userEducationLevel.get();
+    }
+
+    public SimpleStringProperty userEducationLevelProperty() {
+        return userEducationLevel;
+    }
+
+    public void setUserEducationLevel(String userEducationLevel) {
+        this.userEducationLevel.set(userEducationLevel);
+    }
+
+    public String getUserClass() {
+        return userClass.get();
+    }
+
+    public SimpleStringProperty userClassProperty() {
+        return userClass;
+    }
+
+    public void setUserClass(String userClass) {
+        this.userClass.set(userClass);
+    }
+}
---
+++ src/LibraryFunctionality/LibraryUserShowing.java
@@ -0,0 +1,13 @@
+package LibraryFunctionality;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LibraryUserShowing extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
