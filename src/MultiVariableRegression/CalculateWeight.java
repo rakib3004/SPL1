5e516ae159b8420a835bdfb50396e05bf3443839
rakib3004 <bsse1129@iit.du.ac.bd>
2021-08-28 23:43:05 +0600

    print regression value in CalculateWeight.class

--- src/MultiVariableRegression/CalculateWeight.java
+++ src/MultiVariableRegression/CalculateWeight.java
@@ -5,20 +5,25 @@ import ResultPackage.MLR_Result;
 import ObjectOriented.PriorityData;
 public class CalculateWeight {
     double optimizeValue;
+    double isolatedValue=0.0;
     MLR_Result MLRResult = new MLR_Result();
     public  PriorityData[] calculateWeightMethods(double betaZero, double betaOne, double
             betaTwo, double betaThree, PriorityData[] priorityData, int numberOfBooks){
-
+            betaZero=isolatedValue;
         String  className = this.getClass().getSimpleName();
         DateTimeWriter dateTimeWriter =  new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         int iterator;
             for(iterator=0;iterator<numberOfBooks;iterator++){
-            optimizeValue = ((betaOne * priorityData[iterator].borrowPriority)+
-                    (betaTwo * priorityData[iterator].timePriority)+(betaThree * priorityData[iterator].pricePriority));
+
+            optimizeValue = betaZero+(betaOne * priorityData[iterator].borrowPriority)+
+                    (betaTwo * priorityData[iterator].timePriority)+
+                    (betaThree * priorityData[iterator].pricePriority);
+
                 priorityData[iterator].setMLRweight(optimizeValue);
                 priorityData[iterator].setRankValue(optimizeValue);
+                System.out.println(/*optimizeValue+" "+*/priorityData[iterator].borrowPriority/*+" "+priorityData[iterator].timePriority+" "+priorityData[iterator].pricePriority+" "*/);
             }
 return  priorityData;
     }
