16638ced1f7b1aef73d285db1ed8aa441ab126c7
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 00:16:17 +0600

    book-analysis.java  refactoring

--- src/InfoDisplay/BookAnalysis.java
+++ src/InfoDisplay/BookAnalysis.java
@@ -13,11 +13,11 @@ import java.io.IOException;
 public class BookAnalysis extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         Parent parent = FXMLLoader.load(getClass().getResource("database.fxml"));
-primaryStage.setScene(new Scene(parent,1000,500));
-primaryStage.show();
+        primaryStage.setScene(new Scene(parent, 1000, 500));
+        primaryStage.show();
     }
 }
