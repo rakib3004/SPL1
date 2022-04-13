a64706b58ab2142c6ee5f25d366c2f7e7e921a20
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-13 23:38:59 +0600

    price  collection.java  refactoring

--- src/Collection/PriceCollection.java
+++ src/Collection/PriceCollection.java
@@ -9,15 +9,15 @@ import java.util.*;
 public class PriceCollection {
     MedianCalculation medianCalculation = new MedianCalculation();
 
-    public void priceCollectionMLRMethods(PriorityData[] priorityData, int numberOfBooks){
+    public void priceCollectionMLRMethods(PriorityData[] priorityData, int numberOfBooks) {
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Set<String> priceID = new HashSet<>();
         int iterator1;
-        for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+        for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
             priceID.add(priorityData[iterator1].bookData.bookPrice);
         }
@@ -26,71 +26,71 @@ public class PriceCollection {
         System.out.println();
         System.out.println();
         System.out.println();
-        int number=0;
-        int writerCount=0;
+        int number = 0;
+        int writerCount = 0;
         Iterator<String> iterator = priceID.iterator();
 
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             String element = iterator.next();
 
-            int count =0;
+            int count = 0;
 
-            number =1;
+            number = 1;
             writerCount++;
 
             List<Double> list = new ArrayList<>();
 
-
-            System.out.println("##########Books of \""+element+"\" #########("+writerCount+")###");
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
-                    System.out.println(number+" . "+priorityData[iterator1].bookData.bookName);
+            System.out.println("##########Books of \"" + element + "\" #########(" + writerCount + ")###");
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
+                    System.out.println(number + " . " + priorityData[iterator1].bookData.bookName);
                     list.add(priorityData[iterator1].MLRweight);
                     number++;
                     count++;
                 }
             }
-            double summation=0.0;
+            double summation = 0.0;
 
             int sizeB = list.size();
-            if(sizeB>7){
+            if (sizeB > 7) {
                 medianCalculation.medianCalculationMethods(list);
 
             }
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
                     summation = summation + priorityData[iterator1].MLRweight;
                 }
             }
-            double mean = summation/number;
+            double mean = summation / number;
 
-            double standard_deviation=0.0;
+            double standard_deviation = 0.0;
 
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
 
-                    standard_deviation = standard_deviation + ((mean-priorityData[iterator1].MLRweight)*
-                            (mean-priorityData[iterator1].MLRweight));
+                    standard_deviation = standard_deviation + ((mean - priorityData[iterator1].MLRweight) *
+                            (mean - priorityData[iterator1].MLRweight));
                 }
             }
 
-            standard_deviation = standard_deviation/(number-1);
+            standard_deviation = standard_deviation / (number - 1);
             standard_deviation = Math.sqrt(standard_deviation);
-            System.out.println("Mean : "+mean+"    "+"Standard Deviation : "+standard_deviation);
+            System.out.println("Mean : " + mean + "    " + "Standard Deviation : " + standard_deviation);
 
         }
     }
-    public void priceCollectionAHPMethods(PriorityData[] priorityData, int numberOfBooks){
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    public void priceCollectionAHPMethods(PriorityData[] priorityData, int numberOfBooks) {
+
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Set<String> priceID = new HashSet<>();
         int iterator1;
-        for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+        for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
             priceID.add(priorityData[iterator1].bookData.bookPrice);
         }
 
@@ -98,110 +98,111 @@ public class PriceCollection {
         System.out.println();
         System.out.println();
         System.out.println();
-        int number=0;
-        int writerCount=0;
+        int number = 0;
+        int writerCount = 0;
         Iterator<String> iterator = priceID.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             String element = iterator.next();
-            int count =0;
+            int count = 0;
 
-            number =1;
+            number = 1;
             writerCount++;
 
             List<Double> list = new ArrayList<>();
-            System.out.println("##########Books of \""+element+"\" #########("+writerCount+")###");
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
-                    System.out.println(number+" . "+priorityData[iterator1].bookData.bookName);
+            System.out.println("##########Books of \"" + element + "\" #########(" + writerCount + ")###");
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
+                    System.out.println(number + " . " + priorityData[iterator1].bookData.bookName);
                     list.add(priorityData[iterator1].AHPweight);
                     number++;
                     count++;
                 }
             }
 
-            double summation=0.0;
+            double summation = 0.0;
             int sizeB = list.size();
-            if(sizeB>7){
+            if (sizeB > 7) {
                 medianCalculation.medianCalculationMethods(list);
             }
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
                     summation = summation + priorityData[iterator1].AHPweight;
                 }
             }
-            double mean = summation/number;
-            double standard_deviation=0.0;
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
-                    standard_deviation = standard_deviation + ((mean-priorityData[iterator1].AHPweight)*
-                            (mean-priorityData[iterator1].AHPweight));
+            double mean = summation / number;
+            double standard_deviation = 0.0;
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
+                    standard_deviation = standard_deviation + ((mean - priorityData[iterator1].AHPweight) *
+                            (mean - priorityData[iterator1].AHPweight));
                 }
             }
-            standard_deviation = standard_deviation/(number-1);
+            standard_deviation = standard_deviation / (number - 1);
             standard_deviation = Math.sqrt(standard_deviation);
-            System.out.println("Mean : "+mean+"    "+"Standard Deviation : "+standard_deviation);
+            System.out.println("Mean : " + mean + "    " + "Standard Deviation : " + standard_deviation);
         }
     }
-    public void priceCollectionPRAMethods(PriorityData[] priorityData, int numberOfBooks){
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    public void priceCollectionPRAMethods(PriorityData[] priorityData, int numberOfBooks) {
+
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Set<String> priceID = new HashSet<>();
         int iterator1;
 
-        for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+        for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
             priceID.add(priorityData[iterator1].bookData.bookPrice);
-       }
+        }
         System.out.println(priceID.size());
         System.out.println();
         System.out.println();
         System.out.println();
-        int number=0;
-        int writerCount=0;
+        int number = 0;
+        int writerCount = 0;
         Iterator<String> iterator = priceID.iterator();
 
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             String element = iterator.next();
 
-            int count =0;
-            number =1;
+            int count = 0;
+            number = 1;
             writerCount++;
             List<Double> list = new ArrayList<>();
-            System.out.println("##########Books of \""+element+"\" #########("+writerCount+")###");
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
-                    System.out.println(number+" . "+priorityData[iterator1].bookData.bookName);
+            System.out.println("##########Books of \"" + element + "\" #########(" + writerCount + ")###");
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
+                    System.out.println(number + " . " + priorityData[iterator1].bookData.bookName);
                     list.add(priorityData[iterator1].PRAweight);
                     number++;
                     count++;
                 }
             }
-            double summation=0.0;
+            double summation = 0.0;
             int sizeB = list.size();
-            if(sizeB>7){
+            if (sizeB > 7) {
                 medianCalculation.medianCalculationMethods(list);
             }
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
                     summation = summation + priorityData[iterator1].PRAweight;
                 }
             }
-            double mean = summation/number;
-            double standard_deviation=0.0;
-            for(iterator1=0;iterator1<numberOfBooks;iterator1++){
+            double mean = summation / number;
+            double standard_deviation = 0.0;
+            for (iterator1 = 0; iterator1 < numberOfBooks; iterator1++) {
 
-                if(priorityData[iterator1].bookData.bookPrice.equals(element)){
-                    standard_deviation = standard_deviation + ((mean-priorityData[iterator1].PRAweight)*
-                            (mean-priorityData[iterator1].PRAweight));
+                if (priorityData[iterator1].bookData.bookPrice.equals(element)) {
+                    standard_deviation = standard_deviation + ((mean - priorityData[iterator1].PRAweight) *
+                            (mean - priorityData[iterator1].PRAweight));
                 }
             }
-            standard_deviation = standard_deviation/(number-1);
+            standard_deviation = standard_deviation / (number - 1);
             standard_deviation = Math.sqrt(standard_deviation);
-            System.out.println("Mean : "+mean+"    "+"Standard Deviation : "+standard_deviation);
+            System.out.println("Mean : " + mean + "    " + "Standard Deviation : " + standard_deviation);
         }
     }
 }
