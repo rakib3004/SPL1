cc8f2f9dd07e881add1e034e2f8477189497b233
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 00:11:54 +0600

    date time writer.java  refactoring

--- src/FilePackage/DateTimeWriter.java
+++ src/FilePackage/DateTimeWriter.java
@@ -8,35 +8,36 @@ import java.util.Date;
 
 public class DateTimeWriter {
 
-    int classCounter=0;
-    public void dateTimeWriterInitialMethods(String className){
-        Date dNow = new Date( );
-        SimpleDateFormat ft =
-                new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a");
+    int classCounter = 0;
 
-String FILENAME,string,COUNTER_FILE;
+    public void dateTimeWriterInitialMethods(String className) {
+        Date dNow = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a");
+
+        String FILENAME, string, COUNTER_FILE;
         FILENAME = "history.txt";
-        COUNTER_FILE="ClassCounter.txt";
+        COUNTER_FILE = "ClassCounter.txt";
 
+        string = className + "\t";
+        string = string + ft.format(dNow) + "\n";
 
-        string =  className+"\t";
-        string = string+ ft.format(dNow)+"\n";
-
-    /*   BufferedWriter out = null;
-          try {
-                out = new BufferedWriter(
-                        new FileWriter(FILENAME, true));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                out.write(string);
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
+        /*
+         * BufferedWriter out = null;
+         * try {
+         * out = new BufferedWriter(
+         * new FileWriter(FILENAME, true));
+         * } catch (IOException e) {
+         * e.printStackTrace();
+         * }
+         * try {
+         * out.write(string);
+         * out.close();
+         * } catch (IOException e) {
+         * e.printStackTrace();
+         * }
+         */
         try {
-            FileWriter fileWriter=new FileWriter(FILENAME);
+            FileWriter fileWriter = new FileWriter(FILENAME);
             fileWriter.write(string);
             fileWriter.close();
         } catch (IOException e) {
@@ -55,38 +56,40 @@ String FILENAME,string,COUNTER_FILE;
             e.printStackTrace();
         }
     }
-    public void dateTimeWriterMethods(String className){
-        Date dNow = new Date( );
-        SimpleDateFormat ft =
-                new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a");
 
-String FILENAME,string;
+    public void dateTimeWriterMethods(String className) {
+        Date dNow = new Date();
+        SimpleDateFormat ft = new SimpleDateFormat("E dd.MM.yyyy 'at' hh:mm:ss a");
+
+        String FILENAME, string;
         FILENAME = "history.txt";
-     string =  className+"\t";
-        string = string+ ft.format(dNow)+"\n";
+        string = className + "\t";
+        string = string + ft.format(dNow) + "\n";
 
-     BufferedWriter out = null;
-            try {
-                out = new BufferedWriter(
-                        new FileWriter(FILENAME, true));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                out.write(string);
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-      /*  try {
-            FileWriter fileWriter=new FileWriter(FILENAME);
-            fileWriter.write(string);
-            fileWriter.close();
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(
+                    new FileWriter(FILENAME, true));
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
+        try {
+            out.write(string);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*
+         * try {
+         * FileWriter fileWriter=new FileWriter(FILENAME);
+         * fileWriter.write(string);
+         * fileWriter.close();
+         * } catch (IOException e) {
+         * e.printStackTrace();
+         * }
+         */
 
-   String  COUNTER_FILE="ClassCounter.txt";
+        String COUNTER_FILE = "ClassCounter.txt";
         classCounter++;
         String noAccessClass = Integer.toString(classCounter);
 
