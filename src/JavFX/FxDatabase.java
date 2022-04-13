3a06139d2c41ede50c212dc9bcab59421d6d6c70
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 00:21:37 +0600

    fx-database.java refactoring

--- src/JavFX/FxDatabase.java
+++ src/JavFX/FxDatabase.java
@@ -5,19 +5,22 @@ import FilePackage.DateTimeWriter;
 import java.security.PublicKey;
 
 public class FxDatabase {
-        String   index;
+    String index;
     String bookName;
     String writerName;
     String bookId;
     String borrowCount;
     String priceCount;
-    public FxDatabase(String  index, String bookName, String writerName, String bookId, String borrowCount, String priceCount) {
+
+    public FxDatabase(String index, String bookName, String writerName, String bookId, String borrowCount,
+            String priceCount) {
         this.bookName = bookName;
         this.writerName = writerName;
         this.bookId = bookId;
         this.borrowCount = borrowCount;
         this.priceCount = priceCount;
     }
+
     public FxDatabase(String bookName, String writerName, String bookId, String borrowCount, String priceCount) {
         this.bookName = bookName;
         this.writerName = writerName;
