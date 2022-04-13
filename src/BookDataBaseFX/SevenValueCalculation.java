f6a00c61a619baa78cbbe8bfe9318e61625e5c5b
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-13 23:37:00 +0600

    secen value calculation.java  refactoring

--- src/BookDataBaseFX/SevenValueCalculation.java
+++ src/BookDataBaseFX/SevenValueCalculation.java
@@ -10,94 +10,104 @@ public class SevenValueCalculation {
     public double interQuartileRange;
     int firstQuartile;
     int thirdQuartile;
-    public double [] sevenValueCalculationMethods(List<Double> list){
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    public double[] sevenValueCalculationMethods(List<Double> list) {
+
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
-        
-               double [] sevenValueArray  =  new double[7];
+
+        double[] sevenValueArray = new double[7];
         Collections.sort(list);
         sizeA = list.size();
-       sevenValueArray[0] =  findMinimumValue(list);
-        sevenValueArray[6] =findMaximumValue(list);
-        sevenValueArray[3]= findMedianValue(list);
-        sevenValueArray[2]= findFirstQuartileValue(list);
-        sevenValueArray[4]=  findThirdQuartileValue(list);
+        sevenValueArray[0] = findMinimumValue(list);
+        sevenValueArray[6] = findMaximumValue(list);
+        sevenValueArray[3] = findMedianValue(list);
+        sevenValueArray[2] = findFirstQuartileValue(list);
+        sevenValueArray[4] = findThirdQuartileValue(list);
 
         findInterQuartileRange(list);
-      sevenValueArray[1]=  findLowerAdjacentValue(list);
-        sevenValueArray[5]=  findUpperAdjacentValue(list);
+        sevenValueArray[1] = findLowerAdjacentValue(list);
+        sevenValueArray[5] = findUpperAdjacentValue(list);
         return sevenValueArray;
 
     }
-    public double  findMinimumValue(List<Double> list){
 
-   return    list.get(0);
-    } public double  findFirstQuartileValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
+    public double findMinimumValue(List<Double> list) {
 
-        double q1 = (double) sizeA/4;
-        firstQuartile = (int)q1;
+        return list.get(0);
+    }
+
+    public double findFirstQuartileValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
+
+        double q1 = (double) sizeA / 4;
+        firstQuartile = (int) q1;
 
         double checkFloor = q1 - firstQuartile;
 
-        if(checkFloor>0.5){
+        if (checkFloor > 0.5) {
             firstQuartile++;
         }
 
-        return  list.get(firstQuartile);
+        return list.get(firstQuartile);
 
-    } public double  findMedianValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
-
-        double median,median1,median2;
-        if(sizeA%2==1){
-            median = list.get(sizeA/2);
-        }
-        else{
-            median1  = list.get(sizeA/2);
-            median2 =  list.get((sizeA-1)/2);
-
-            median = (median1 + median2)/2;
-        }
-return  median;
     }
-    public double  findThirdQuartileValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
 
-        double q2 = (3*sizeA)/4;
-        thirdQuartile = (int)q2;
+    public double findMedianValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
+
+        double median, median1, median2;
+        if (sizeA % 2 == 1) {
+            median = list.get(sizeA / 2);
+        } else {
+            median1 = list.get(sizeA / 2);
+            median2 = list.get((sizeA - 1) / 2);
+
+            median = (median1 + median2) / 2;
+        }
+        return median;
+    }
+
+    public double findThirdQuartileValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
+
+        double q2 = (3 * sizeA) / 4;
+        thirdQuartile = (int) q2;
         double checkFloor = q2 - thirdQuartile;
 
-        if(checkFloor>0.5){
+        if (checkFloor > 0.5) {
             thirdQuartile++;
-        }
-        else{
+        } else {
             thirdQuartile++;
             thirdQuartile--;
         }
-        return  list.get(thirdQuartile);
+        return list.get(thirdQuartile);
 
-    } public double  findMaximumValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
-      return list.get(sizeA-1);
     }
-    public void findInterQuartileRange(List<Double> list){
 
-        String  className = this.getClass().getSimpleName();
-                interQuartileRange = list.get(thirdQuartile) -  list.get(firstQuartile) ;
+    public double findMaximumValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
+        return list.get(sizeA - 1);
     }
-    public double findLowerAdjacentValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
+
+    public void findInterQuartileRange(List<Double> list) {
+
+        String className = this.getClass().getSimpleName();
+        interQuartileRange = list.get(thirdQuartile) - list.get(firstQuartile);
+    }
+
+    public double findLowerAdjacentValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
 
         double lowerAdjacentValue = firstQuartile - (1.5 * interQuartileRange);
-  return lowerAdjacentValue;
+        return lowerAdjacentValue;
     }
-    public double findUpperAdjacentValue(List<Double> list){
-        String  className = this.getClass().getSimpleName();
+
+    public double findUpperAdjacentValue(List<Double> list) {
+        String className = this.getClass().getSimpleName();
 
         double upperAdjacentValue = thirdQuartile + (1.5 * interQuartileRange);
-return upperAdjacentValue;
+        return upperAdjacentValue;
     }
 }
