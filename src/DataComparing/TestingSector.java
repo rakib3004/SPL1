30e6901c122968833af6b1647aec2df176e10575
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 00:09:44 +0600

    testing sector.java  refactoring

--- src/DataComparing/TestingSector.java
+++ src/DataComparing/TestingSector.java
@@ -17,10 +17,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestingSector {
-    PriorityData[] priorityData,priorityDataCV,priorityData2;
+    PriorityData[] priorityData, priorityDataCV, priorityData2;
     GenericAlgo[] genericAlgo;
     PrioritySort prioritySort = new PrioritySort();
-    List list = new ArrayList(); 
+    List list = new ArrayList();
     int iterator;
     int numberOfBooks;
     Processing processing = new Processing();
@@ -28,9 +28,9 @@ public class TestingSector {
     AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
     MultipleLinearRegression multipleLinearRegression = new MultipleLinearRegression();
 
-    public PriorityData [] testingSectorMethods(AHPcriteriaWeight ahPcriteriaWeight,int processIndex) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    public PriorityData[] testingSectorMethods(AHPcriteriaWeight ahPcriteriaWeight, int processIndex) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         try {
             priorityData = processing.fileReaderMethods();
@@ -41,28 +41,28 @@ public class TestingSector {
             e.printStackTrace();
         }
 
-
-        if(processIndex==1){
+        if (processIndex == 1) {
             priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
             priorityData2 = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
 
-        }
-        else  if(processIndex==2){
+        } else if (processIndex == 2) {
             AHPcriteriaWeight ahPcriteriaWeight1;
             AHPcriteriaWeight ahPcriteriaWeight2;
             AHPcalculation ahPcalculation = new AHPcalculation();
             AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
-            ahPcriteriaWeight1 =  ahPcalculation.AHPcalculationMethods(priorityData,numberOfBooks);
-            ahPcriteriaWeight2 =  ahPcalculation.AHPcalculationMethods(priorityData,numberOfBooks);
-            priorityData=     ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight1,priorityData,numberOfBooks);
-            priorityData2=     ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight2,priorityData,numberOfBooks);
+            ahPcriteriaWeight1 = ahPcalculation.AHPcalculationMethods(priorityData, numberOfBooks);
+            ahPcriteriaWeight2 = ahPcalculation.AHPcalculationMethods(priorityData, numberOfBooks);
+            priorityData = ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight1, priorityData,
+                    numberOfBooks);
+            priorityData2 = ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight2, priorityData,
+                    numberOfBooks);
 
-        } else  if(processIndex==3){
+        } else if (processIndex == 3) {
 
             PageRankProcessData pageRankProcessData = new PageRankProcessData();
             try {
-                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);
-                priorityData2 = pageRankProcessData.PageRankProcessDataMethods(priorityData,numberOfBooks);
+                priorityData = pageRankProcessData.PageRankProcessDataMethods(priorityData, numberOfBooks);
+                priorityData2 = pageRankProcessData.PageRankProcessDataMethods(priorityData, numberOfBooks);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -70,15 +70,15 @@ public class TestingSector {
         for (iterator = 0; iterator < numberOfBooks; iterator++) {
             if (priorityData[iterator].bookData.bookId.substring(13, 14).contains("5") ||
                     priorityData[iterator].bookData.bookId.substring(13, 14).contains("0")) {
-             // System.out.println(priorityData[iterator].getMLRweight());
+                // System.out.println(priorityData[iterator].getMLRweight());
                 if (Integer.parseInt(priorityData[iterator].bookData.bookPrice) <= 180) {
-                    //System.out.println(iterator + ":::: 180 er niche dam");
+                    // System.out.println(iterator + ":::: 180 er niche dam");
                     priorityData[iterator].pricePriority = ahPcriteriaWeight.lowPrice;
                 } else if (Integer.parseInt(priorityData[iterator].bookData.bookPrice) <= 250) {
-                    //System.out.println(iterator + ":::: 250 er niche dam");
+                    // System.out.println(iterator + ":::: 250 er niche dam");
                     priorityData[iterator].pricePriority = ahPcriteriaWeight.mediumPrice;
                 } else {
-                    //System.out.println(iterator + ":::: Besi dam");
+                    // System.out.println(iterator + ":::: Besi dam");
                     priorityData[iterator].pricePriority = ahPcriteriaWeight.highPrice;
                 }
             }
@@ -87,19 +87,19 @@ public class TestingSector {
             if (priorityData[iterator].bookData.bookId.substring(13, 14).contains("5") ||
                     priorityData[iterator].bookData.bookId.substring(13, 14).contains("0")) {
                 if (priorityData[iterator].timePriority <= 4.00) {
-                    //System.out.println(iterator + "::::latest book");
+                    // System.out.println(iterator + "::::latest book");
                     priorityData[iterator].timePriority = ahPcriteriaWeight.latestBook;
                 } else if (priorityData[iterator].timePriority <= 6.00) {
-                    //System.out.println(iterator + "::::newly book");
+                    // System.out.println(iterator + "::::newly book");
                     priorityData[iterator].timePriority = ahPcriteriaWeight.newlyBook;
                 } else if (priorityData[iterator].timePriority <= 8.00) {
-                    //System.out.println(iterator + "::::recently old book");
+                    // System.out.println(iterator + "::::recently old book");
                     priorityData[iterator].timePriority = ahPcriteriaWeight.recentlyOldBook;
                 } else if (priorityData[iterator].timePriority <= 10.00) {
-                    //System.out.println(iterator + "::::old book");
+                    // System.out.println(iterator + "::::old book");
                     priorityData[iterator].timePriority = ahPcriteriaWeight.oldBook;
                 } else if (priorityData[iterator].timePriority <= 12.00) {
-                    //System.out.println(iterator + "::::oldest book");
+                    // System.out.println(iterator + "::::oldest book");
                     priorityData[iterator].timePriority = ahPcriteriaWeight.oldestBook;
                 }
             }
@@ -109,18 +109,18 @@ public class TestingSector {
                     priorityData[iterator].bookData.bookId.substring(13, 14).contains("0")) {
 
                 if (priorityData[iterator].borrowPriority <= 10) {
-                    //System.out.println(iterator + ":::: 10 er niche");
+                    // System.out.println(iterator + ":::: 10 er niche");
                     priorityData[iterator].borrowPriority = ahPcriteriaWeight.lowlyDemand;
                 } else if (priorityData[iterator].borrowPriority <= 20) {
-                    //System.out.println(iterator + ":::: 20 er niche");
+                    // System.out.println(iterator + ":::: 20 er niche");
 
                     priorityData[iterator].borrowPriority = ahPcriteriaWeight.lowMediumDemand;
                 } else if (priorityData[iterator].borrowPriority <= 30) {
-                    //System.out.println(iterator + ":::: 30 er niche");
+                    // System.out.println(iterator + ":::: 30 er niche");
 
                     priorityData[iterator].borrowPriority = ahPcriteriaWeight.highMediumDemand;
                 } else if (priorityData[iterator].borrowPriority <= 40) {
-                    //System.out.println(iterator + ":::: 40 er niche");
+                    // System.out.println(iterator + ":::: 40 er niche");
 
                     priorityData[iterator].borrowPriority = ahPcriteriaWeight.highlyDemand;
                 }
@@ -131,42 +131,42 @@ public class TestingSector {
                     priorityData[iterator].bookData.bookId.substring(13, 14).contains("0")) {
 
                 if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("01")) {
-                    //System.out.println(iterator + ":::: Uponnash");
+                    // System.out.println(iterator + ":::: Uponnash");
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.uponnashType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("05")) {
-                    //System.out.println(iterator + ":::: Rochonaboli");
+                    // System.out.println(iterator + ":::: Rochonaboli");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.uponnashType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("02")) {
-                    //System.out.println(iterator + ":::: Kobita");
+                    // System.out.println(iterator + ":::: Kobita");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.kobitaType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("13")) {
-                    //System.out.println(iterator + ":::: Biggan");
+                    // System.out.println(iterator + ":::: Biggan");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.scienceFictionType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("14")) {
-                    //System.out.println(iterator + ":::: Gobeshona");
+                    // System.out.println(iterator + ":::: Gobeshona");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.scienceFictionType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("06")) {
-                    //System.out.println(iterator + ":::: Science Fiction");
+                    // System.out.println(iterator + ":::: Science Fiction");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.scienceFictionType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("10")) {
-                    //System.out.println(iterator + ":::: Kisore Uponnash");
+                    // System.out.println(iterator + ":::: Kisore Uponnash");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.kisorUponnashType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("11")) {
-                    //System.out.println(iterator + ":::: Shishu Shahitto");
+                    // System.out.println(iterator + ":::: Shishu Shahitto");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.kisorUponnashType;
                 } else if (priorityData[iterator].bookData.bookId.substring(0, 2).equals("04")) {
-                    //System.out.println(iterator + ":::: Religion");
+                    // System.out.println(iterator + ":::: Religion");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.religionType;
                 } else {
-                    //System.out.println(iterator + ":::: Othres");
+                    // System.out.println(iterator + ":::: Othres");
 
                     priorityData[iterator].bookPriority = ahPcriteriaWeight.othersType;
                 }
