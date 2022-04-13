3d08cdbd03623b7e7dc2d149aa8ebb7628319381
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 00:19:35 +0600

    seven-value-statistics.java  refactoring

--- src/InfoDisplay/SevenValueStatistics.java
+++ src/InfoDisplay/SevenValueStatistics.java
@@ -39,43 +39,44 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class SevenValueStatistics extends Application {
-   PriorityData[] priorityData;
+    PriorityData[] priorityData;
     int numberOfBooks;
     Processing processing = new Processing();
     BookNumber bookNumber = new BookNumber();
     DoublyLinearRegression doublyLinearRegression = new DoublyLinearRegression();
-SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementation();
+    SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementation();
+
     @Override
     public void start(Stage primaryStage) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         Font font = new Font(19);
         Font font1 = new Font(47);
-        Font font2 = new Font( 23);
+        Font font2 = new Font(23);
 
         Button priceBased = new Button("Price Based");
         Button typeBased = new Button("Type Based");
         Button timeBased = new Button("Time Based");
         Button borrowBased = new Button("Borrow Based");
 
-        typeBased.setPrefSize(400,170);
-        timeBased.setPrefSize(400,170);
-        borrowBased.setPrefSize(400,170);
-        priceBased.setPrefSize(400,170);
+        typeBased.setPrefSize(400, 170);
+        timeBased.setPrefSize(400, 170);
+        borrowBased.setPrefSize(400, 170);
+        priceBased.setPrefSize(400, 170);
 
         typeBased.setFont(font2);
         timeBased.setFont(font2);
         borrowBased.setFont(font2);
         priceBased.setFont(font2);
-        
+
         typeBased.setTranslateX(30);
         typeBased.setTranslateY(35);
         timeBased.setTranslateX(30);
         timeBased.setTranslateY(210);
         borrowBased.setTranslateX(30);
         borrowBased.setTranslateY(385);
-         priceBased.setTranslateX(30);
+        priceBased.setTranslateX(30);
         priceBased.setTranslateY(560);
 
         typeBased.setContentDisplay(ContentDisplay.LEFT);
@@ -85,7 +86,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         FileInputStream fileInputStream1 = null;
         try {
             fileInputStream1 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"types.png");
+                    "src" + File.separator + "Images" + File.separator + "types.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -95,7 +96,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         FileInputStream fileInputStream2 = null;
         try {
             fileInputStream2 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"time.png");
+                    "src" + File.separator + "Images" + File.separator + "time.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -105,7 +106,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         FileInputStream fileInputStream3 = null;
         try {
             fileInputStream3 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"borrow.png");
+                    "src" + File.separator + "Images" + File.separator + "borrow.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -115,7 +116,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         FileInputStream fileInputStream7 = null;
         try {
             fileInputStream7 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"demand.png");
+                    "src" + File.separator + "Images" + File.separator + "demand.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -155,7 +156,6 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             }
         });
 
-
         Button exit = new Button("Exit");
         exit.setTranslateX(1200);
         exit.setTranslateY(700);
@@ -165,7 +165,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         FileInputStream fileInputStream4 = null;
         try {
             fileInputStream4 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"exit.png");
+                    "src" + File.separator + "Images" + File.separator + "exit.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -185,13 +185,13 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         Button home = new Button("Home");
         home.setTranslateX(470);
         home.setTranslateY(520);
-        home.setPrefSize(375,30);
+        home.setPrefSize(375, 30);
         home.setFont(font2);
         home.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream5 = null;
         try {
             fileInputStream5 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"home.png");
+                    "src" + File.separator + "Images" + File.separator + "home.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -201,21 +201,20 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             Main main = new Main();
             try {
                 main.start(primaryStage);
-            }
-            catch (Exception exception){
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
         Button back = new Button("Back");
         back.setTranslateX(950);
         back.setTranslateY(520);
-        back.setPrefSize(375,30);
+        back.setPrefSize(375, 30);
         back.setFont(font2);
         back.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream6 = null;
         try {
             fileInputStream6 = new FileInputStream(
-                    "src"+ File.separator +"Images"+ File.separator +"back.png");
+                    "src" + File.separator + "Images" + File.separator + "back.png");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -225,21 +224,20 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             ProcessAnalysis processAnalysis = new ProcessAnalysis();
             try {
                 processAnalysis.start(primaryStage);
-            }
-            catch (Exception exception){
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
-        Image background = new Image("Images"+ File.separator +"framework.jpg");
+        Image background = new Image("Images" + File.separator + "framework.jpg");
         Canvas canvas = new Canvas(850, 425);
         canvas.setTranslateX(470);
         canvas.setTranslateY(35);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(background,0,0);
+        graphicsContext.drawImage(background, 0, 0);
         Group group = new Group();
-        group.getChildren().addAll(canvas,typeBased,
-                timeBased,priceBased,
-                borrowBased,exit,home,text,back);
+        group.getChildren().addAll(canvas, typeBased,
+                timeBased, priceBased,
+                borrowBased, exit, home, text, back);
         ReadingRoom readingRoom2 = new ReadingRoom();
         LibraryDesk libraryDesk2 = new LibraryDesk();
         CrossValidation crossValidation2 = new CrossValidation();
@@ -260,7 +258,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
 
         MenuItem menuItem1a = new MenuItem("Default Recommendation");
         MenuItem menuItem1b = new MenuItem("User Based Recommendation");
-        menu1.getItems().addAll(menuItem1a,menuItem1b);
+        menu1.getItems().addAll(menuItem1a, menuItem1b);
         menuItem1a.setOnAction(actionEvent -> {
             try {
                 readingRoom2.automaticSystem(primaryStage);
@@ -275,7 +273,6 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
                 e.printStackTrace();
             }
         });
-
 
         MenuItem menuItem2a = new MenuItem("Add book");
         MenuItem menuItem2b = new MenuItem("Book Info");
@@ -302,7 +299,7 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             }
         });
 
-        menu2.getItems().addAll(menuItem2a,menuItem2b,menuItem2c);
+        menu2.getItems().addAll(menuItem2a, menuItem2b, menuItem2c);
 
         MenuItem menuItem3a = new MenuItem("Cross Validation");
         MenuItem menuItem3b = new MenuItem("Seven Number Analysis");
@@ -322,15 +319,15 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             }
         });
         menuItem3c.setOnAction(actionEvent -> {
-            //     writerCollection.writerCollectionMLRMethods();
+            // writerCollection.writerCollectionMLRMethods();
         });
 
-        menu3.getItems().addAll(menuItem3a,menuItem3b,menuItem3c);
+        menu3.getItems().addAll(menuItem3a, menuItem3b, menuItem3c);
 
         MenuItem menuItem4a = new MenuItem("Multi-variable Regression");
         MenuItem menuItem4b = new MenuItem("Analytic Hierarchy Process");
         MenuItem menuItem4c = new MenuItem("Page Rank Algorithm");
-        menu4.getItems().addAll(menuItem4a,menuItem4b,menuItem4c);
+        menu4.getItems().addAll(menuItem4a, menuItem4b, menuItem4c);
         menuItem4a.setOnAction(actionEvent -> {
             try {
                 fourVariableRegression1.start(primaryStage);
@@ -355,7 +352,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
 
         MenuItem readingRoom1 = new MenuItem("Reading Room");
         readingRoom1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 ReadingRoom readingRoom = new ReadingRoom();
                 try {
                     {
@@ -368,7 +366,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem libraryDesk1 = new MenuItem("Library Desk");
         libraryDesk1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 LibraryDesk libraryDesk = new LibraryDesk();
                 try {
                     {
@@ -382,7 +381,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         MenuItem processVisualization = new MenuItem("Process Visualization");
 
         processVisualization.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 ProcessImplementation processImplementation = new ProcessImplementation();
                 {
                     try {
@@ -395,7 +395,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem multivariableLinearRegression = new MenuItem("Multi-variable Linear Regression");
         multivariableLinearRegression.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 FourVariableRegression fourVariableRegression = new FourVariableRegression();
                 try {
                     {
@@ -408,7 +409,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem analyticHierarchyProcess = new MenuItem("AnalyticHierarchy Process");
         analyticHierarchyProcess.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 AnalyticHierarchyAlgorithm analyticHierarchyAlgorithm = new AnalyticHierarchyAlgorithm();
                 try {
                     {
@@ -421,7 +423,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem pageRankAlgorithm = new MenuItem("PageRank Algorithm");
         pageRankAlgorithm.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 PageRankAlgorithm pageRankAlgorithm = new PageRankAlgorithm();
                 try {
                     {
@@ -434,7 +437,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem crossValidationProcess = new MenuItem("Cross ValidationProcess");
         crossValidationProcess.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 CrossValidation crossValidation = new CrossValidation();
                 {
                     try {
@@ -447,7 +451,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         });
         MenuItem trainingSetView = new MenuItem("Training Set View");
         trainingSetView.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 TrainingSet trainingSet = new TrainingSet();
                 {
                     trainingSet.start(primaryStage);
@@ -457,7 +462,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
 
         MenuItem testingSetView = new MenuItem("Testing Set View");
         testingSetView.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 TestingSet testingSet = new TestingSet();
                 {
                     testingSet.start(primaryStage);
@@ -468,7 +474,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
         MenuItem bookInformationView = new MenuItem("Book Information");
 
         bookInformationView.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 BookInformationShow bookInformationShow = new BookInformationShow();
                 {
                     try {
@@ -482,7 +489,8 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
 
         MenuItem systemAnalysis = new MenuItem("System Analysis");
         systemAnalysis.setOnAction(new EventHandler<ActionEvent>() {
-            @Override public void handle(ActionEvent e) {
+            @Override
+            public void handle(ActionEvent e) {
                 ProcessAnalysis processAnalysis = new ProcessAnalysis();
                 {
                     try {
@@ -494,29 +502,29 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
             }
         });
 
-        menu5.getItems().addAll(readingRoom1,libraryDesk1,
-                processVisualization,multivariableLinearRegression,
-                analyticHierarchyProcess,pageRankAlgorithm,
-                crossValidationProcess,trainingSetView,
-                testingSetView,bookInformationView,systemAnalysis);
+        menu5.getItems().addAll(readingRoom1, libraryDesk1,
+                processVisualization, multivariableLinearRegression,
+                analyticHierarchyProcess, pageRankAlgorithm,
+                crossValidationProcess, trainingSetView,
+                testingSetView, bookInformationView, systemAnalysis);
 
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(menu1,menu2,menu3,menu4,menu5);
+        menuBar.getMenus().addAll(menu1, menu2, menu3, menu4, menu5);
         menuBar.setStyle("-fx-text-fill: #000000;");
         menuBar.setStyle("-fx-background-color: #ffffff;");
         menuBar.setStyle("-fx-padding: 1 5 1 5;");
         menuBar.setStyle(" -fx-spacing: 47;");
         menuBar.prefHeight(32);
         group.getChildren().add(menuBar);
-        Scene scene1 = new Scene(group,1500,950);
+        Scene scene1 = new Scene(group, 1500, 950);
         menuBar.prefWidthProperty().bind(scene1.widthProperty());
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
-    public Button setStyle( Button button)
-    {
+
+    public Button setStyle(Button button) {
         button.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -528,10 +536,10 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  button;
+        return button;
     }
-    public MenuButton setStyle( MenuButton menuButton)
-    {
+
+    public MenuButton setStyle(MenuButton menuButton) {
         menuButton.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -543,6 +551,6 @@ SevenNumberImplementation sevenNumberImplementation = new SevenNumberImplementat
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  menuButton;
+        return menuButton;
     }
 }
