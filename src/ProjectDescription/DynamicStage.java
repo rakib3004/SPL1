e386ba70abf2871c048488af63770d6d6dba9890
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:58:02 +0600

    dynamic-stage.java refactoring

--- src/ProjectDescription/DynamicStage.java
+++ src/ProjectDescription/DynamicStage.java
@@ -24,11 +24,12 @@ import java.io.File;
 public class DynamicStage extends Application {
     Timeline time = new Timeline();
     int timeSec = 1;
-    int bookCounter=0;
+    int bookCounter = 0;
+
     @Override
     public void start(Stage primaryStage) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         Button back = new Button("Back");
         Button exit = new Button("Exit");
@@ -55,9 +56,9 @@ public class DynamicStage extends Application {
 
         });
         exit.setOnAction(actionEvent -> {
-           System.exit(0);
+            System.exit(0);
         });
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
+        Font font3 = Font.font(Font.getFontNames().get(0), FontWeight.BOLD, 30);
         back.setFont(font3);
         exit.setFont(font3);
 
@@ -77,76 +78,71 @@ public class DynamicStage extends Application {
                 new KeyFrame(Duration.seconds(2),
                         event -> {
                             timeSec++;
-                            if(timeSec%4==1){
+                            if (timeSec % 4 == 1) {
 
-
-                                Image image = new Image("Images"+ File.separator +"Slide2.jpg");
-                                Canvas canvas = new Canvas(1500,950);
+                                Image image = new Image("Images" + File.separator + "Slide2.jpg");
+                                Canvas canvas = new Canvas(1500, 950);
                                 Group group = new Group();
-                                group.getChildren().addAll(canvas,exit,back,bookNameText);
+                                group.getChildren().addAll(canvas, exit, back, bookNameText);
 
                                 GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-                                graphicsContext.drawImage(image,0,0);
+                                graphicsContext.drawImage(image, 0, 0);
 
-                                Scene scene1 = new Scene(group,1500,950);
+                                Scene scene1 = new Scene(group, 1500, 950);
+                                primaryStage.setScene(scene1);
+                                primaryStage.setTitle("Recommendation Tool");
+                                primaryStage.setFullScreen(true);
+                                primaryStage.show();
+                            } else if (timeSec % 4 == 2) {
+
+                                Image image = new Image("Images" + File.separator + "Slide3.jpg");
+                                Canvas canvas = new Canvas(1500, 950);
+                                Group group = new Group();
+                                group.getChildren().addAll(canvas, exit, back, bookNameText);
+
+                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+                                graphicsContext.drawImage(image, 0, 0);
+                                Scene scene1 = new Scene(group, 1500, 950);
+                                primaryStage.setScene(scene1);
+                                primaryStage.setTitle("Recommendation Tool");
+                                primaryStage.setFullScreen(true);
+                                primaryStage.show();
+
+                            } else if (timeSec % 4 == 3) {
+                                Image image = new Image("Images" + File.separator + "Slide4.jpg");
+                                Canvas canvas = new Canvas(1500, 950);
+                                Group group = new Group();
+                                group.getChildren().addAll(canvas, exit, back, bookNameText);
+
+                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+                                graphicsContext.drawImage(image, 0, 0);
+
+                                Scene scene1 = new Scene(group, 1500, 950);
+
+                                primaryStage.setScene(scene1);
+                                primaryStage.setTitle("Recommendation Tool");
+                                primaryStage.setFullScreen(true);
+                                primaryStage.show();
+                            } else if (timeSec % 4 == 0) {
+
+                                Image image = new Image("Images" + File.separator + "Slide5.jpg");
+                                Canvas canvas = new Canvas(1500, 950);
+                                Group group = new Group();
+                                group.getChildren().addAll(canvas, exit, back, bookNameText);
+                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+                                graphicsContext.drawImage(image, 0, 0);
+                                Scene scene1 = new Scene(group, 1500, 950);
+
                                 primaryStage.setScene(scene1);
                                 primaryStage.setTitle("Recommendation Tool");
                                 primaryStage.setFullScreen(true);
                                 primaryStage.show();
                             }
-                            else  if(timeSec%4==2){
-
-                                Image image = new Image("Images"+ File.separator +"Slide3.jpg");
-                                Canvas canvas = new Canvas(1500,950);
-                                Group group = new Group();
-                                group.getChildren().addAll(canvas,exit,back,bookNameText);
-
-                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-                                graphicsContext.drawImage(image,0,0);
-                                Scene scene1 = new Scene(group,1500,950);
-                                primaryStage.setScene(scene1);
-                                primaryStage.setTitle("Recommendation Tool");
-                                primaryStage.setFullScreen(true);
-                                primaryStage.show();
-
-                            }  else  if(timeSec%4==3){
-                                Image image = new Image("Images"+ File.separator +"Slide4.jpg");
-                                Canvas canvas = new Canvas(1500,950);
-                                Group group = new Group();
-                                group.getChildren().addAll(canvas,exit,back,bookNameText);
-
-                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-                                graphicsContext.drawImage(image,0,0);
-
-                                Scene scene1 = new Scene(group,1500,950);
-
-
-                                primaryStage.setScene(scene1);
-                                primaryStage.setTitle("Recommendation Tool");
-                                primaryStage.setFullScreen(true);
-                                primaryStage.show();
-                            }   else  if(timeSec%4==0){
-
-
-                                Image image = new Image("Images"+ File.separator +"Slide5.jpg");
-                                Canvas canvas = new Canvas(1500,950);
-                                Group group = new Group();
-                                group.getChildren().addAll(canvas,exit,back,bookNameText);
-                                GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-                                graphicsContext.drawImage(image,0,0);
-                                Scene scene1 = new Scene(group,1500,950);
-
-                                primaryStage.setScene(scene1);
-                                primaryStage.setTitle("Recommendation Tool");
-                                primaryStage.setFullScreen(true);
-                                primaryStage.show();
-                            }
-                        })
-        );
+                        }));
         time.play();
     }
-    public Text setStyle(Text text)
-    {
+
+    public Text setStyle(Text text) {
         text.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -158,8 +154,7 @@ public class DynamicStage extends Application {
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  text;
+        return text;
     }
-
 
 }
