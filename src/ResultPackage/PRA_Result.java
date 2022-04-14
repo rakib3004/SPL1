b3e7d561a72020a3d7a9fb3c70d19d4b7510152d
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-15 00:28:25 +0600

    pra-result.java refactoring

--- src/ResultPackage/PRA_Result.java
+++ src/ResultPackage/PRA_Result.java
@@ -10,7 +10,6 @@ import ObjectOriented.PriorityData;
 import javax.swing.*;
 import java.util.Scanner;
 
-
 public class PRA_Result {
     Searching searching = new Searching();
     WriterCollection writerCollection = new WriterCollection();
@@ -21,11 +20,12 @@ public class PRA_Result {
     ReverseSorting reverseSorting = new ReverseSorting();
     PrintInfo printInfo = new PrintInfo();
     int iterator;
+
     public void PRA_ResultMethods(PriorityData[] priorityData, int numberOfBooks) {
         int enterChoice;
         Scanner scanner = new Scanner(System.in);
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         System.out.println("Enter Your Choice : ");
         System.out.println("1. Show Result");
@@ -36,34 +36,29 @@ public class PRA_Result {
         System.out.println("6. Time Based Analysis");
         System.out.println("7. borrow Based Analysis");
         enterChoice = scanner.nextInt();
-        if(enterChoice==1){
-            printInfo.showPRAProcessResult(priorityData,numberOfBooks);
-        }
-        else if(enterChoice==2){
-            reverseSorting.sortingPRAmethods(priorityData,numberOfBooks);
-        }
-        else if(enterChoice==3){
-            timeCollection.timeCollectionPRAMethods(priorityData,numberOfBooks);
-        }
-        else if(enterChoice==4){
-            writerCollection.writerCollectionPRAMethods(priorityData,numberOfBooks);
-        }
-        else if (enterChoice==5){
-            priceCollection.priceCollectionPRAMethods(priorityData,numberOfBooks);
-        }
-        else if (enterChoice==6){
-            timeCollection.timeCollectionPRAMethods(priorityData,numberOfBooks);
-        }
-        else if (enterChoice==7){
-            borrowCollection.borrowCollectionPRAMethods(priorityData,numberOfBooks);
+        if (enterChoice == 1) {
+            printInfo.showPRAProcessResult(priorityData, numberOfBooks);
+        } else if (enterChoice == 2) {
+            reverseSorting.sortingPRAmethods(priorityData, numberOfBooks);
+        } else if (enterChoice == 3) {
+            timeCollection.timeCollectionPRAMethods(priorityData, numberOfBooks);
+        } else if (enterChoice == 4) {
+            writerCollection.writerCollectionPRAMethods(priorityData, numberOfBooks);
+        } else if (enterChoice == 5) {
+            priceCollection.priceCollectionPRAMethods(priorityData, numberOfBooks);
+        } else if (enterChoice == 6) {
+            timeCollection.timeCollectionPRAMethods(priorityData, numberOfBooks);
+        } else if (enterChoice == 7) {
+            borrowCollection.borrowCollectionPRAMethods(priorityData, numberOfBooks);
         }
     }
-    public void printOptionMethods(PriorityData[] priorityData, int numberOfBooks){
+
+    public void printOptionMethods(PriorityData[] priorityData, int numberOfBooks) {
         double temporary;
         int temp;
         PriorityData tempData;
-            try {
-                String aString = JOptionPane.showInputDialog("Chose OPtion :\n1.Type Base Statitics " +
+        try {
+            String aString = JOptionPane.showInputDialog("Chose OPtion :\n1.Type Base Statitics " +
                     "\n2.Writer Based Statistics \n3.Issue Date Base Statitics " +
                     "\n4.Borrow Based Statistics" + "\n5.Searching by Alphabets");
             int anInt = Integer.parseInt(aString);
