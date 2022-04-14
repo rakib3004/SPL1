1b24523855a83c6cdfd76e2a0a47499c7056fe49
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-15 00:00:03 +0600

    process-imp.java refactoring

--- src/RecommendationAlgorithm/ProcessImplementation.java
+++ src/RecommendationAlgorithm/ProcessImplementation.java
@@ -24,24 +24,23 @@ import java.io.FileNotFoundException;
 
 public class ProcessImplementation extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Font font = new Font(19);
         Font font1 = new Font(47);
-        Font font2 = new Font( 26);
+        Font font2 = new Font(26);
         Button multiVariableRegression = new Button("MultiVariable Regression");
         Button analyticHierarchyProcess1 = new Button("Analytic Hierarchy Process");
         Button pageRankAlgorithm = new Button("Page Rank Algorithm");
 
-        multiVariableRegression.setPrefSize(410,230);
-        analyticHierarchyProcess1.setPrefSize(410,230);
-        pageRankAlgorithm.setPrefSize(410,230);
+        multiVariableRegression.setPrefSize(410, 230);
+        analyticHierarchyProcess1.setPrefSize(410, 230);
+        pageRankAlgorithm.setPrefSize(410, 230);
 
         multiVariableRegression.setFont(font2);
         analyticHierarchyProcess1.setFont(font2);
@@ -57,26 +56,29 @@ public class ProcessImplementation extends Application {
         analyticHierarchyProcess1.setContentDisplay(ContentDisplay.TOP);
         pageRankAlgorithm.setContentDisplay(ContentDisplay.TOP);
         FileInputStream fileInputStream1 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"4var.png");
+                "src" + File.separator + "Images" + File.separator + "4var.png");
         Image image1 = new Image(fileInputStream1);
         multiVariableRegression.setGraphic(new ImageView(image1));
 
         FileInputStream fileInputStream2 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"ahp.png");
+                "src" + File.separator + "Images" + File.separator + "ahp.png");
         Image image2 = new Image(fileInputStream2);
         analyticHierarchyProcess1.setGraphic(new ImageView(image2));
 
         FileInputStream fileInputStream3 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"pra.png");
+                "src" + File.separator + "Images" + File.separator + "pra.png");
         Image image3 = new Image(fileInputStream3);
         pageRankAlgorithm.setGraphic(new ImageView(image3));
         multiVariableRegression.setOnAction(actionEvent -> {
-         /*   MultiVariableRegression multiVaribleRegression = new MultiVariableRegression();
-            try {
-                multiVaribleRegression.start(primaryStage);
-            } catch (Exception exception) {
-                exception.printStackTrace();
-            }*/
+            /*
+             * MultiVariableRegression multiVaribleRegression = new
+             * MultiVariableRegression();
+             * try {
+             * multiVaribleRegression.start(primaryStage);
+             * } catch (Exception exception) {
+             * exception.printStackTrace();
+             * }
+             */
             FourVariableRegression fourVariableRegression1 = new FourVariableRegression();
             try {
                 fourVariableRegression1.start(primaryStage);
@@ -108,7 +110,7 @@ public class ProcessImplementation extends Application {
             System.exit(0);
         });
         FileInputStream fileInputStream4 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"exit.png");
+                "src" + File.separator + "Images" + File.separator + "exit.png");
         Image image4 = new Image(fileInputStream4);
         exit.setGraphic(new ImageView(image4));
         exit.setPrefSize(120, 20);
@@ -125,30 +127,29 @@ public class ProcessImplementation extends Application {
         Button home = new Button("Home");
         home.setTranslateX(470);
         home.setTranslateY(520);
-        home.setPrefSize(375,30);
+        home.setPrefSize(375, 30);
         home.setFont(font2);
         home.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream5 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"home.png");
+                "src" + File.separator + "Images" + File.separator + "home.png");
         Image image5 = new Image(fileInputStream5);
         home.setGraphic(new ImageView(image5));
         home.setOnAction(actionEvent -> {
             Main main = new Main();
             try {
                 main.start(primaryStage);
-            }
-            catch (Exception exception){
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
         Button back = new Button("Back");
         back.setTranslateX(950);
         back.setTranslateY(520);
-        back.setPrefSize(375,30);
+        back.setPrefSize(375, 30);
         back.setFont(font2);
         back.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream6 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"back.png");
+                "src" + File.separator + "Images" + File.separator + "back.png");
         Image image6 = new Image(fileInputStream6);
         back.setGraphic(new ImageView(image6));
         back.setOnAction(actionEvent -> {
@@ -159,24 +160,23 @@ public class ProcessImplementation extends Application {
                 exception.printStackTrace();
             }
         });
-        Image background = new Image("Images"+ File.separator +"framework.jpg");
+        Image background = new Image("Images" + File.separator + "framework.jpg");
         Canvas canvas = new Canvas(850, 425);
         canvas.setTranslateX(470);
         canvas.setTranslateY(35);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(background,0,0);
+        graphicsContext.drawImage(background, 0, 0);
         Group group = new Group();
-        group.getChildren().addAll(canvas,multiVariableRegression,
-                analyticHierarchyProcess1,exit,back,home,text,pageRankAlgorithm);
-        Scene scene1 = new Scene(group,1400,800);
+        group.getChildren().addAll(canvas, multiVariableRegression,
+                analyticHierarchyProcess1, exit, back, home, text, pageRankAlgorithm);
+        Scene scene1 = new Scene(group, 1400, 800);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
 
-    public Button setStyle( Button button)
-    {
+    public Button setStyle(Button button) {
         button.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -188,6 +188,6 @@ public class ProcessImplementation extends Application {
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  button;
+        return button;
     }
 }
