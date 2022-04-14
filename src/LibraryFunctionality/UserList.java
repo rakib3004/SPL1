1c0262d6c71eb423c4a155546cc89f82edd691ac
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:35:30 +0600

    user-list.java refactoring

--- src/LibraryFunctionality/UserList.java
+++ src/LibraryFunctionality/UserList.java
@@ -4,86 +4,88 @@ import java.io.*;
 
 public class UserList {
 
-    public void addUser(String textUserID,String [] infoDataArray) throws IOException {
+    public void addUser(String textUserID, String[] infoDataArray) throws IOException {
 
-File file =new File("src"+File.separator+"LibraryFunctionality" +File.separator+"UserTable.txt");
+        File file = new File("src" + File.separator + "LibraryFunctionality" + File.separator + "UserTable.txt");
 
-
-  /*      FileWriter fileWriter = new FileWriter(file);
-        //user id------user name------user institute -------  higher/secondary---- class no/dept name
-        fileWriter.write(textUserID+"\t"+infoDataArray[0]+"\t"+infoDataArray[1]+"\t"+infoDataArray[3]+"\t"+infoDataArray[2]+"\n");
-        fileWriter.close();
-        */
+        /*
+         * FileWriter fileWriter = new FileWriter(file);
+         * //user id------user name------user institute ------- higher/secondary----
+         * class no/dept name
+         * fileWriter.write(textUserID+"\t"+infoDataArray[0]+"\t"+infoDataArray[1]+"\t"+
+         * infoDataArray[3]+"\t"+infoDataArray[2]+"\n");
+         * fileWriter.close();
+         */
 
         BufferedWriter out = null;
-          try {
-                out = new BufferedWriter(
-                        new FileWriter("src"+File.separator+"LibraryFunctionality" +File.separator+"UserTable.txt", true));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                out.write(textUserID+"\t"+infoDataArray[0]+"\t"+infoDataArray[1]+"\t"+infoDataArray[3]+"\t"+infoDataArray[2]+"\n");
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        try {
+            out = new BufferedWriter(
+                    new FileWriter("src" + File.separator + "LibraryFunctionality" + File.separator + "UserTable.txt",
+                            true));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            out.write(textUserID + "\t" + infoDataArray[0] + "\t" + infoDataArray[1] + "\t" + infoDataArray[3] + "\t"
+                    + infoDataArray[2] + "\n");
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    char[] userInfoData = new char[5000];
 
-char [] userInfoData = new char[5000];
+    LibraryUser[] libraryUser = new LibraryUser[1000];
 
-    LibraryUser [] libraryUser = new LibraryUser[1000];
-    public  LibraryUser [] parseUser()throws IOException {
-        File file =new File("src"+File.separator+"LibraryFunctionality" +File.separator+"UserTable.txt");
+    public LibraryUser[] parseUser() throws IOException {
+        File file = new File("src" + File.separator + "LibraryFunctionality" + File.separator + "UserTable.txt");
         FileReader fileReader = new FileReader(file);
         fileReader.read(userInfoData);
         fileReader.close();
-        int userIterator=0;
-         String userID;
-         String userName;
-         String userInstitute;
-         String userEducationLevel;
-         String userClass;
-        String [] dataArray = new String[5];
-        String dataCollector="";
-        int dataIterator=0;
-        int arrayIterator=0;
-        for(dataIterator=0;userInfoData[dataIterator]!='\0';dataIterator++){
+        int userIterator = 0;
+        String userID;
+        String userName;
+        String userInstitute;
+        String userEducationLevel;
+        String userClass;
+        String[] dataArray = new String[5];
+        String dataCollector = "";
+        int dataIterator = 0;
+        int arrayIterator = 0;
+        for (dataIterator = 0; userInfoData[dataIterator] != '\0'; dataIterator++) {
 
-            if(userInfoData[dataIterator]=='\t'){
+            if (userInfoData[dataIterator] == '\t') {
                 dataCollector = dataCollector.trim();
-                dataArray[arrayIterator]=dataCollector;
+                dataArray[arrayIterator] = dataCollector;
                 arrayIterator++;
-                dataCollector="";
-            }
-            else if(userInfoData[dataIterator]=='\n'){
+                dataCollector = "";
+            } else if (userInfoData[dataIterator] == '\n') {
                 dataCollector = dataCollector.trim();
-                dataArray[arrayIterator]=dataCollector;
-                dataCollector="";
+                dataArray[arrayIterator] = dataCollector;
+                dataCollector = "";
                 userID = dataArray[0];
                 userName = dataArray[1];
                 userInstitute = dataArray[2];
                 userEducationLevel = dataArray[3];
                 userClass = dataArray[4];
-                libraryUser[userIterator] = new LibraryUser(userID,userName,userInstitute,userEducationLevel,userClass);
-                System.out.println(userID+"\t"+userName+"\t"+userInstitute+"\t"+userEducationLevel+"\t"+userClass);
-                dataArray[0]="";
-                dataArray[1]="";
-                dataArray[2]="";
-                dataArray[3]="";
-                dataArray[4]="";
-                arrayIterator=0;
+                libraryUser[userIterator] = new LibraryUser(userID, userName, userInstitute, userEducationLevel,
+                        userClass);
+                System.out.println(
+                        userID + "\t" + userName + "\t" + userInstitute + "\t" + userEducationLevel + "\t" + userClass);
+                dataArray[0] = "";
+                dataArray[1] = "";
+                dataArray[2] = "";
+                dataArray[3] = "";
+                dataArray[4] = "";
+                arrayIterator = 0;
 
-            }
-            else {
-               dataCollector = dataCollector+userInfoData[dataIterator];
+            } else {
+                dataCollector = dataCollector + userInfoData[dataIterator];
             }
         }
-return  libraryUser;
+        return libraryUser;
     }
-
 
 }
