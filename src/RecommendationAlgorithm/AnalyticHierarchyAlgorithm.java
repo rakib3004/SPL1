e9c9a62fde9b16bf3396ea38731f841e3cffd8e3
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:59:07 +0600

    analytic-hierarchy-algorithm.java refactoring

--- src/RecommendationAlgorithm/AnalyticHierarchyAlgorithm.java
+++ src/RecommendationAlgorithm/AnalyticHierarchyAlgorithm.java
@@ -37,27 +37,26 @@ public class AnalyticHierarchyAlgorithm extends Application {
     Processing processing = new Processing();
     BookNumber bookNumber = new BookNumber();
 
-AHPcalculation ahPcalculation = new AHPcalculation();
+    AHPcalculation ahPcalculation = new AHPcalculation();
     AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
 
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Font font = new Font(19);
         Font font1 = new Font(47);
-        Font font2 = new Font( 26);
+        Font font2 = new Font(26);
         Button results = new Button("Results");
         Button crossValidation = new Button("Cross Validation");
         Button description = new Button("Description");
 
-
-        results.setPrefSize(410,230);
-        crossValidation.setPrefSize(410,230);
-        description.setPrefSize(410,230);
+        results.setPrefSize(410, 230);
+        crossValidation.setPrefSize(410, 230);
+        description.setPrefSize(410, 230);
 
         results.setFont(font2);
         crossValidation.setFont(font2);
@@ -73,39 +72,40 @@ AHPcalculation ahPcalculation = new AHPcalculation();
         crossValidation.setContentDisplay(ContentDisplay.TOP);
         description.setContentDisplay(ContentDisplay.TOP);
         FileInputStream fileInputStream1 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"table.png");
+                "src" + File.separator + "Images" + File.separator + "table.png");
         Image image1 = new Image(fileInputStream1);
         results.setGraphic(new ImageView(image1));
 
         FileInputStream fileInputStream2 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"graph.png");
+                "src" + File.separator + "Images" + File.separator + "graph.png");
         Image image2 = new Image(fileInputStream2);
         crossValidation.setGraphic(new ImageView(image2));
 
         FileInputStream fileInputStream3 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"console.png");
+                "src" + File.separator + "Images" + File.separator + "console.png");
         Image image3 = new Image(fileInputStream3);
         description.setGraphic(new ImageView(image3));
         description.setOnAction(actionEvent -> {
             ProjectDescription projectDescription = new ProjectDescription();
             try {
-                projectDescription.start(primaryStage,5);
-            }
-            catch (Exception exception){
+                projectDescription.start(primaryStage, 5);
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
         crossValidation.setOnAction(actionEvent -> {
-           /* AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
-            try {
-                ahp_chart_view.start(primaryStage);
-            }
-            catch (Exception exception) {
-                exception.printStackTrace();
-            }*/
+            /*
+             * AHP_Chart_View ahp_chart_view = new AHP_Chart_View();
+             * try {
+             * ahp_chart_view.start(primaryStage);
+             * }
+             * catch (Exception exception) {
+             * exception.printStackTrace();
+             * }
+             */
             CrossValidationShowing crossValidationShowing = new CrossValidationShowing();
             try {
-                crossValidationShowing.start(primaryStage,2);
+                crossValidationShowing.start(primaryStage, 2);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
@@ -115,8 +115,7 @@ AHPcalculation ahPcalculation = new AHPcalculation();
             try {
 
                 ahpTableViewFX.start(primaryStage);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
@@ -128,7 +127,7 @@ AHPcalculation ahPcalculation = new AHPcalculation();
             System.exit(0);
         });
         FileInputStream fileInputStream4 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"exit.png");
+                "src" + File.separator + "Images" + File.separator + "exit.png");
         Image image4 = new Image(fileInputStream4);
         exit.setGraphic(new ImageView(image4));
         exit.setPrefSize(120, 20);
@@ -145,60 +144,58 @@ AHPcalculation ahPcalculation = new AHPcalculation();
         Button home = new Button("Home");
         home.setTranslateX(470);
         home.setTranslateY(520);
-        home.setPrefSize(375,30);
+        home.setPrefSize(375, 30);
         home.setFont(font2);
         home.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream5 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"home.png");
+                "src" + File.separator + "Images" + File.separator + "home.png");
         Image image5 = new Image(fileInputStream5);
         home.setGraphic(new ImageView(image5));
         home.setOnAction(actionEvent -> {
             Main main = new Main();
             try {
                 main.start(primaryStage);
-            }
-            catch (Exception exception){
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
         Button back = new Button("Back");
         back.setTranslateX(950);
         back.setTranslateY(520);
-        back.setPrefSize(375,30);
+        back.setPrefSize(375, 30);
         back.setFont(font2);
         back.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream6 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"back.png");
+                "src" + File.separator + "Images" + File.separator + "back.png");
         Image image6 = new Image(fileInputStream6);
         back.setGraphic(new ImageView(image6));
         back.setOnAction(actionEvent -> {
             try {
                 ProcessImplementation processImplementation1 = new ProcessImplementation();
                 processImplementation1.start(primaryStage);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
 
         });
-        Image background = new Image("Images"+ File.separator +"framework.jpg");
+        Image background = new Image("Images" + File.separator + "framework.jpg");
         Canvas canvas = new Canvas(850, 425);
         canvas.setTranslateX(470);
         canvas.setTranslateY(35);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(background,0,0);
+        graphicsContext.drawImage(background, 0, 0);
         Group group = new Group();
-        group.getChildren().addAll(canvas,results,
-                crossValidation,exit,back,home,text,description);
+        group.getChildren().addAll(canvas, results,
+                crossValidation, exit, back, home, text, description);
 
-        Scene scene1 = new Scene(group,1500,950);
+        Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
-    public Button setStyle( Button button)
-    {
+
+    public Button setStyle(Button button) {
         button.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -210,6 +207,6 @@ AHPcalculation ahPcalculation = new AHPcalculation();
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  button;
+        return button;
     }
 }
