0d4c5415ff7ee4c2c1a5a3d0cceee767e3c87ed3
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-15 00:44:18 +0600

    analytical-chart.java  refactoring

--- src/VisualRepresentation/AnalyticChart.java
+++ src/VisualRepresentation/AnalyticChart.java
@@ -27,10 +27,10 @@ public class AnalyticChart extends Application {
     public void start(Stage primaryStage) {
     }
 
-    public void startTypeBook(Stage primaryStage){
+    public void startTypeBook(Stage primaryStage) {
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         Button back = new Button("Back");
         Button exit = new Button("Exit");
@@ -59,68 +59,87 @@ public class AnalyticChart extends Application {
 
         NumberAxis numberAxis = new NumberAxis();
         numberAxis.setLabel("Generics of Book");
-        BarChart barChart = new BarChart(categoryAxis,numberAxis);
+        BarChart barChart = new BarChart(categoryAxis, numberAxis);
 
         XYChart.Series series1 = new XYChart.Series();
         series1.setName("Number of Books");
-        String year2008,year2009,year2010,year2011,year2012,year2013,year2014,year2015,year2016,year2017;
-        int  year2008Books,year2009Books,year2010Books,year2011Books,year2012Books,
-                year2013Books,year2014Books,year2015Books,year2016Books,year2017Books;
-        year2008Books=0;year2009Books=0;year2010Books=0;year2011Books=0;
-        year2012Books=0;year2013Books=0;year2014Books=0;year2015Books=0;
-        year2016Books=0;year2017Books=0;
+        String year2008, year2009, year2010, year2011, year2012, year2013, year2014, year2015, year2016, year2017;
+        int year2008Books, year2009Books, year2010Books, year2011Books, year2012Books,
+                year2013Books, year2014Books, year2015Books, year2016Books, year2017Books;
+        year2008Books = 0;
+        year2009Books = 0;
+        year2010Books = 0;
+        year2011Books = 0;
+        year2012Books = 0;
+        year2013Books = 0;
+        year2014Books = 0;
+        year2015Books = 0;
+        year2016Books = 0;
+        year2017Books = 0;
         int iterator;
-        year2008 =  "year2008" ;
-        year2009 =  "year2009" ; year2010 =  "year2010" ; year2011 =  "year2011" ;
-        year2012=   "year2012" ;year2013 =  "year2013" ; year2014 =  "year2014" ;
-        year2015 =  "year2015" ;year2016 =  "year2016" ; year2017 =  "year2017" ;
-        series1.getData().add(new XYChart.Data(year2008,year2008Books));
-        series1.getData().add(new XYChart.Data(year2009,year2009Books));
-        series1.getData().add(new XYChart.Data(year2010,year2010Books));
-        series1.getData().add(new XYChart.Data(year2011,year2011Books));
-        series1.getData().add(new XYChart.Data(year2012,year2012Books));
-        series1.getData().add(new XYChart.Data(year2013,year2013Books));
-        series1.getData().add(new XYChart.Data(year2014,year2014Books));
-        series1.getData().add(new XYChart.Data(year2015,year2015Books));
-        series1.getData().add(new XYChart.Data(year2016,year2016Books));
-        series1.getData().add(new XYChart.Data(year2017,year2017Books));
+        year2008 = "year2008";
+        year2009 = "year2009";
+        year2010 = "year2010";
+        year2011 = "year2011";
+        year2012 = "year2012";
+        year2013 = "year2013";
+        year2014 = "year2014";
+        year2015 = "year2015";
+        year2016 = "year2016";
+        year2017 = "year2017";
+        series1.getData().add(new XYChart.Data(year2008, year2008Books));
+        series1.getData().add(new XYChart.Data(year2009, year2009Books));
+        series1.getData().add(new XYChart.Data(year2010, year2010Books));
+        series1.getData().add(new XYChart.Data(year2011, year2011Books));
+        series1.getData().add(new XYChart.Data(year2012, year2012Books));
+        series1.getData().add(new XYChart.Data(year2013, year2013Books));
+        series1.getData().add(new XYChart.Data(year2014, year2014Books));
+        series1.getData().add(new XYChart.Data(year2015, year2015Books));
+        series1.getData().add(new XYChart.Data(year2016, year2016Books));
+        series1.getData().add(new XYChart.Data(year2017, year2017Books));
 
         barChart.getData().add(series1);
         barChart.setTranslateX(65);
         barChart.setTranslateY(55);
-        barChart.setPrefSize(500,500);
+        barChart.setPrefSize(500, 500);
         barChart.getData().add(series1);
         barChart.setTranslateX(65);
         barChart.setTranslateY(55);
-        barChart.setPrefSize(500,500);
+        barChart.setPrefSize(500, 500);
 
-
-        Image background = new Image("Images"+ File.separator +"libraryBackground4.jpg");
+        Image background = new Image("Images" + File.separator + "libraryBackground4.jpg");
         BackgroundImage bi = new BackgroundImage(background,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundRepeat.NO_REPEAT,
                 BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         Background bg = new Background(bi);
-        Group group = new Group(exit,back);
-        Scene scene = new Scene(group ,1400, 770);
+        Group group = new Group(exit, back);
+        Scene scene = new Scene(group, 1400, 770);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
-    }public void startDemandBook(Stage primaryStage){
+    }
 
-    }public void startGenericBook(Stage primaryStage){
-
-    }public void startCLassBook(Stage primaryStage){
-
-    }public void startBorrowCount(Stage primaryStage){
+    public void startDemandBook(Stage primaryStage) {
 
     }
 
-    public Button setStyle(Button button)
-    {
+    public void startGenericBook(Stage primaryStage) {
+
+    }
+
+    public void startCLassBook(Stage primaryStage) {
+
+    }
+
+    public void startBorrowCount(Stage primaryStage) {
+
+    }
+
+    public Button setStyle(Button button) {
         button.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -132,6 +151,6 @@ public class AnalyticChart extends Application {
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  button;
+        return button;
     }
 }
