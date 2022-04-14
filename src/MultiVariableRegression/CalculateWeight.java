1b61b2f9aa1548756fc15117791cdeb6fbd01c4f
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:42:31 +0600

    calculate-weight.java refactoring

--- src/MultiVariableRegression/CalculateWeight.java
+++ src/MultiVariableRegression/CalculateWeight.java
@@ -3,28 +3,31 @@ package MultiVariableRegression;
 import FilePackage.DateTimeWriter;
 import ResultPackage.MLR_Result;
 import ObjectOriented.PriorityData;
+
 public class CalculateWeight {
     double optimizeValue;
-    double isolatedValue=0.0;
+    double isolatedValue = 0.0;
     MLR_Result MLRResult = new MLR_Result();
-    public  PriorityData[] calculateWeightMethods(double betaZero, double betaOne, double
-            betaTwo, double betaThree, PriorityData[] priorityData, int numberOfBooks){
-            betaZero=isolatedValue;
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+
+    public PriorityData[] calculateWeightMethods(double betaZero, double betaOne, double betaTwo, double betaThree,
+            PriorityData[] priorityData, int numberOfBooks) {
+        betaZero = isolatedValue;
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         int iterator;
-            for(iterator=0;iterator<numberOfBooks;iterator++){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
 
-            optimizeValue = betaZero+(betaOne * priorityData[iterator].borrowPriority)+
-                    (betaTwo * priorityData[iterator].timePriority)+
+            optimizeValue = betaZero + (betaOne * priorityData[iterator].borrowPriority) +
+                    (betaTwo * priorityData[iterator].timePriority) +
                     (betaThree * priorityData[iterator].pricePriority);
 
-                priorityData[iterator].setMLRweight(optimizeValue);
-                priorityData[iterator].setRankValue(optimizeValue);
-                System.out.println(/*optimizeValue+" "+*/priorityData[iterator].borrowPriority/*+" "+priorityData[iterator].timePriority+" "+priorityData[iterator].pricePriority+" "*/);
-            }
-return  priorityData;
+            priorityData[iterator].setMLRweight(optimizeValue);
+            priorityData[iterator].setRankValue(optimizeValue);
+            System.out.println(priorityData[iterator].borrowPriority);
+
+        }
+        return priorityData;
     }
 }
