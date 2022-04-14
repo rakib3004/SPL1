00886a518279593ccbd97effcabd488b146ad7f8
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:35:06 +0600

    user-choice-table-data.java refactoring

--- src/LibraryFunctionality/UserChoiceTableData.java
+++ src/LibraryFunctionality/UserChoiceTableData.java
@@ -50,10 +50,11 @@ public class UserChoiceTableData {
     BookNumber bookNumber = new BookNumber();
     String labelName;
     MultipleLinearRegression multipleLinearRegression = new MultipleLinearRegression();
-PriorityList priorityList1= new PriorityList();
-    public void start(Stage primaryStage,String userName, String userID,PriorityList priorityList){
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    PriorityList priorityList1 = new PriorityList();
+
+    public void start(Stage primaryStage, String userName, String userID, PriorityList priorityList) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         try {
             priorityData = processing.fileReaderMethods();
@@ -61,8 +62,8 @@ PriorityList priorityList1= new PriorityList();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData,numberOfBooks);
-        priorityData = prioritySort.PrioritySortingMLRmethods(priorityData,numberOfBooks);
+        priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
+        priorityData = prioritySort.PrioritySortingMLRmethods(priorityData, numberOfBooks);
         Button back = new Button("Back");
         Button exit = new Button("Exit");
         back.setTranslateX(0);
@@ -82,64 +83,65 @@ PriorityList priorityList1= new PriorityList();
         });
         back.setPrefSize(200, 80);
         exit.setPrefSize(200, 80);
-                labelName="  "+userName+"'s Choice List";
-                int jterator=0;
-                for(jterator=0;jterator<priorityList.typeCounter;jterator++){
-                    for (iterator = 0; iterator < numberOfBooks; iterator++) {
-                        if (priorityData[iterator].bookData.typeName.equals(priorityList.favTypeList[jterator])) {
-                            list.add(new Book(priorityData[iterator].bookData.bookName,
-                                    priorityData[iterator].bookData.writerName,
-                                    priorityData[iterator].bookData.bookId,
-                                    priorityData[iterator].bookData.typeName));
-                            data = FXCollections.observableList(list);
-                            //Kisore Uponnash
-                        }
-                        if(priorityData[iterator].bookData.typeName.equals("Uponnash")||priorityData[iterator].bookData.typeName.equals("Sci Fi")||priorityData[iterator].bookData.typeName.equals("Kisore Uponnash")){
-                            list.add(new Book(priorityData[iterator].bookData.bookName,
-                                    priorityData[iterator].bookData.writerName,
-                                    priorityData[iterator].bookData.bookId,
-                                    priorityData[iterator].bookData.typeName));
-                            data = FXCollections.observableList(list);
-                        }
-                    }
-
+        labelName = "  " + userName + "'s Choice List";
+        int jterator = 0;
+        for (jterator = 0; jterator < priorityList.typeCounter; jterator++) {
+            for (iterator = 0; iterator < numberOfBooks; iterator++) {
+                if (priorityData[iterator].bookData.typeName.equals(priorityList.favTypeList[jterator])) {
+                    list.add(new Book(priorityData[iterator].bookData.bookName,
+                            priorityData[iterator].bookData.writerName,
+                            priorityData[iterator].bookData.bookId,
+                            priorityData[iterator].bookData.typeName));
+                    data = FXCollections.observableList(list);
+                    // Kisore Uponnash
                 }
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
+                if (priorityData[iterator].bookData.typeName.equals("Uponnash")
+                        || priorityData[iterator].bookData.typeName.equals("Sci Fi")
+                        || priorityData[iterator].bookData.typeName.equals("Kisore Uponnash")) {
+                    list.add(new Book(priorityData[iterator].bookData.bookName,
+                            priorityData[iterator].bookData.writerName,
+                            priorityData[iterator].bookData.bookId,
+                            priorityData[iterator].bookData.typeName));
+                    data = FXCollections.observableList(list);
+                }
+            }
+
+        }
+        Font font3 = Font.font(Font.getFontNames().get(0), FontWeight.BOLD, 30);
         back.setFont(font3);
         exit.setFont(font3);
         Button show = new Button("Show");
         show.setTranslateX(550);
         show.setTranslateY(350);
-        show.setPrefSize(200,100);
+        show.setPrefSize(200, 100);
         show.setFont(font3);
 
         show.setOnAction(actionEvent -> {
-    showInfo(primaryStage,labelName,data);
-});
+            showInfo(primaryStage, labelName, data);
+        });
 
-
-
-        Image image = new Image("Images"+ File.separator +"Form2.jpg");
-        Canvas canvas = new Canvas(1500,950);
+        Image image = new Image("Images" + File.separator + "Form2.jpg");
+        Canvas canvas = new Canvas(1500, 950);
         Group group = new Group();
-        group.getChildren().addAll(canvas,exit,back,show/*,bookType*/);
+        group.getChildren().addAll(canvas, exit, back, show/* ,bookType */);
 
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(image,0,0);
-        Scene scene1 = new Scene(group,1500,950);
+        graphicsContext.drawImage(image, 0, 0);
+        Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
 
     }
-    public void showInfo(Stage secondaryStage,String labelName,ObservableList data){
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+
+    public void showInfo(Stage secondaryStage, String labelName, ObservableList data) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Label label = new Label();
-        label.setPrefSize(350,45);
+        label.setPrefSize(350, 45);
         label.setTranslateX(460);
         label.setTranslateY(15);
         label.setText(labelName);
@@ -150,16 +152,16 @@ PriorityList priorityList1= new PriorityList();
         back.setTranslateY(650);
         exit.setTranslateX(1100);
         exit.setTranslateY(650);
-        Font font3= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,30);
-        Font font4= Font.font(Font.getFontNames().get(0), FontWeight.BOLD,40);
+        Font font3 = Font.font(Font.getFontNames().get(0), FontWeight.BOLD, 30);
+        Font font4 = Font.font(Font.getFontNames().get(0), FontWeight.BOLD, 40);
         back.setFont(font3);
         exit.setFont(font3);
         label.setFont(font4);
-        String userName="", userID="";
+        String userName = "", userID = "";
         back.setOnAction(actionEvent -> {
             list.clear();
             try {
-                this.start(secondaryStage,userName,userID,priorityList1);
+                this.start(secondaryStage, userName, userID, priorityList1);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
@@ -185,7 +187,7 @@ PriorityList priorityList1= new PriorityList();
         TableColumn typeName = new TableColumn("Type Name");
         typeName.setCellValueFactory(new PropertyValueFactory("typeName"));
 
-        table.getColumns().setAll(bookName,writerName,typeName,bookId);
+        table.getColumns().setAll(bookName, writerName, typeName, bookId);
         table.setPrefWidth(1240);
         table.setPrefHeight(560);
         table.setTranslateX(60);
@@ -196,10 +198,10 @@ PriorityList priorityList1= new PriorityList();
         table.getSelectionModel().select(0);
         Book book = (Book) table.getSelectionModel().getSelectedItem();
 
-        Image image = new Image("Images"+ File.separator +"libraryBackground1.jpg");
+        Image image = new Image("Images" + File.separator + "libraryBackground1.jpg");
         Canvas canvas = new Canvas(1500, 950);
         Group group = new Group();
-        group.getChildren().addAll(canvas,exit, back,label,table);
+        group.getChildren().addAll(canvas, exit, back, label, table);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
         graphicsContext.drawImage(image, 0, 0);
 
@@ -219,17 +221,17 @@ PriorityList priorityList1= new PriorityList();
     }
 
     private ObservableList getInitialTableData() throws IOException {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         List list = new ArrayList();
 
         priorityData = processing.fileReaderMethods();
         numberOfBooks = bookNumber.bookNumberFindingMethods();
-        priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData,numberOfBooks);
-        priorityData = prioritySort.PrioritySortingMLRmethods(priorityData,numberOfBooks);
+        priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
+        priorityData = prioritySort.PrioritySortingMLRmethods(priorityData, numberOfBooks);
         int iterator;
-        for(iterator=0;iterator<numberOfBooks;iterator++){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
             list.add(new Book(priorityData[genericAlgo[iterator].getIndex()].bookData.bookName,
                     priorityData[genericAlgo[iterator].getIndex()].bookData.writerName,
                     priorityData[genericAlgo[iterator].getIndex()].bookData.bookId));
@@ -237,6 +239,5 @@ PriorityList priorityList1= new PriorityList();
         ObservableList data = FXCollections.observableList(list);
         return data;
     }
-
 
 }
