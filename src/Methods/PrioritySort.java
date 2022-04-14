69c822c59acb76d5503fe68341aacefc9cb41910
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:40:16 +0600

    priority-sort.java refactoring

--- src/Methods/PrioritySort.java
+++ src/Methods/PrioritySort.java
@@ -5,16 +5,17 @@ import ObjectOriented.GenericAlgo;
 import ObjectOriented.PriorityData;
 
 public class PrioritySort {
-    int iterator,jterator;
-    public   PriorityData [] PrioritySortingMLRmethods(PriorityData[] priorityData, int numberOfBooks){
+    int iterator, jterator;
 
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    public PriorityData[] PrioritySortingMLRmethods(PriorityData[] priorityData, int numberOfBooks) {
+
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         PriorityData temporaryPriority;
-        for(iterator = 0; iterator <numberOfBooks; iterator++){
-            for(jterator= iterator+1;jterator<numberOfBooks;jterator++){
-                if(priorityData[iterator].getMLRweight()<priorityData[jterator].getMLRweight()){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            for (jterator = iterator + 1; jterator < numberOfBooks; jterator++) {
+                if (priorityData[iterator].getMLRweight() < priorityData[jterator].getMLRweight()) {
                     temporaryPriority = priorityData[jterator];
                     priorityData[jterator] = priorityData[iterator];
                     priorityData[iterator] = temporaryPriority;
