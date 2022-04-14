545e68b9300904dfb69a5fd7c57e75deea93c369
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:55:45 +0600

    generic-algo.java refactoring

--- src/ObjectOriented/GenericAlgo.java
+++ src/ObjectOriented/GenericAlgo.java
@@ -3,20 +3,25 @@ package ObjectOriented;
 public class GenericAlgo {
     double weight;
     int index;
-    public GenericAlgo(double weight , int index){
+
+    public GenericAlgo(double weight, int index) {
         this.weight = weight;
         this.index = index;
     }
-    public double getWeight(){
-        return  weight;
+
+    public double getWeight() {
+        return weight;
     }
-    public void setWeight(double weight){
+
+    public void setWeight(double weight) {
         this.weight = weight;
     }
-    public int getIndex(){
-        return  index;
+
+    public int getIndex() {
+        return index;
     }
-    public void setIndex(int index){
+
+    public void setIndex(int index) {
         this.index = index;
     }
 
