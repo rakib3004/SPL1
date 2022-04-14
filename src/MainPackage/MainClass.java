839c2d0f64e80c1e3eaff11a051f2da502493242
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:37:28 +0600

    main-class.java refactoring

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -1,28 +1,26 @@
 
 package MainPackage;
 
-        import AHPalgorithm.AHPcalculation;
-        import AHPalgorithm.AHPprocessImplementation;
-        import SimpleRegression.DoublyLinearRegression;
-        import MultiVariableRegression.MultipleLinearRegression;
-        import ObjectOriented.AHPcriteriaWeight;
-        import ObjectOriented.PriorityData;
-        import PageRankAlgorithm.PageRankCalculation;
-        import Regression.TypeCountRegression;
-        import ResultPackage.AHP_Result;
-        import ResultPackage.MLR_Result;
-        import ResultPackage.PRA_Result;
+import AHPalgorithm.AHPcalculation;
+import AHPalgorithm.AHPprocessImplementation;
+import SimpleRegression.DoublyLinearRegression;
+import MultiVariableRegression.MultipleLinearRegression;
+import ObjectOriented.AHPcriteriaWeight;
+import ObjectOriented.PriorityData;
+import PageRankAlgorithm.PageRankCalculation;
+import Regression.TypeCountRegression;
+import ResultPackage.AHP_Result;
+import ResultPackage.MLR_Result;
+import ResultPackage.PRA_Result;
 
-        import javax.swing.*;
+import javax.swing.*;
 
-        import java.io.IOException;
-        import java.util.Scanner;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MainClass extends JFrame {
 
-
-    public static void main(String[] args)throws IOException {
-
+    public static void main(String[] args) throws IOException {
 
         PriorityData[] priorityData;
         AHPcriteriaWeight ahPcriteriaWeight;
@@ -38,51 +36,46 @@ public class MainClass extends JFrame {
         AHPcalculation ahPcalculation = new AHPcalculation();
         AHPprocessImplementation ahPprocessImplementation = new AHPprocessImplementation();
         Scanner scanner = new Scanner(System.in);
-        int input1,input2;
+        int input1, input2;
 
         System.out.println("What do you want???\n1.Multivariable Linear Regression \n" +
                 "2.Analytic Heirarcy Process\n3.Page Rank Algorithm");
         input1 = scanner.nextInt();
         priorityData = processing.fileReaderMethods();
         numberOfBooks = bookNumber.bookNumberFindingMethods();
-        if(input1==1){
+        if (input1 == 1) {
 
             System.out.println("What do you want???\n1.Two Variable Linear Regression \n" +
                     "2.Three Variable Linear Regression \n3.Four Variable Linear Regression ");
             input2 = scanner.nextInt();
 
-            if(input2==1){
-                typeCountRegression.typeCountRegressionMethods(priorityData,numberOfBooks);
-            }
-            else if(input2==2){
-                doublyLinearRegression.doublyLinearRegressionMethods(priorityData,numberOfBooks);
+            if (input2 == 1) {
+                typeCountRegression.typeCountRegressionMethods(priorityData, numberOfBooks);
+            } else if (input2 == 2) {
+                doublyLinearRegression.doublyLinearRegressionMethods(priorityData, numberOfBooks);
 
+            } else if (input2 == 3) {
+                priorityData = multipleLinearRegression.multipleLinearRegressionMethods(priorityData, numberOfBooks);
+                mlrResult.MLR_ResultMethods(priorityData, numberOfBooks);
             }
-            else if(input2==3){
-                priorityData =    multipleLinearRegression.multipleLinearRegressionMethods(priorityData,numberOfBooks);
-                mlrResult.MLR_ResultMethods(priorityData,numberOfBooks);
-            }
-        }
-        else if(input1==2){
-            ahPcriteriaWeight =  ahPcalculation.AHPcalculationMethods(priorityData,numberOfBooks);
-       priorityData=     ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight,priorityData,numberOfBooks);
-             ahpResult.AHP_ResultMethods(priorityData,numberOfBooks);
-        }
-        else if(input1==6){
+        } else if (input1 == 2) {
+            ahPcriteriaWeight = ahPcalculation.AHPcalculationMethods(priorityData, numberOfBooks);
+            priorityData = ahPprocessImplementation.ahpProcessImplementationMethods(ahPcriteriaWeight, priorityData,
+                    numberOfBooks);
+            ahpResult.AHP_ResultMethods(priorityData, numberOfBooks);
+        } else if (input1 == 6) {
             int i;
-            for(i=0;i<numberOfBooks;i++){
+            for (i = 0; i < numberOfBooks; i++) {
                 System.out.println(priorityData[i].timePriority);
             }
-        }
-        else{
+        } else {
             PageRankCalculation pageRankCalculation = new PageRankCalculation();
             try {
 
-           priorityData=   pageRankCalculation.pageRankCalculationMethods(priorityData,numberOfBooks);
-praResult.PRA_ResultMethods(priorityData,numberOfBooks);
+                priorityData = pageRankCalculation.pageRankCalculationMethods(priorityData, numberOfBooks);
+                praResult.PRA_ResultMethods(priorityData, numberOfBooks);
                 System.exit(0);
-            }
-            catch (Exception exception) {
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         }
