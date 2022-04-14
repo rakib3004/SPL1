a09f681e71747652de8f443b836db22e60fbe3ea
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:59:23 +0600

    mlr.java refactoring

--- src/RecommendationAlgorithm/MultiVariableRegression.java
+++ src/RecommendationAlgorithm/MultiVariableRegression.java
@@ -25,23 +25,22 @@ import java.io.FileNotFoundException;
 
 public class MultiVariableRegression extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         Font font = new Font(19);
         Font font1 = new Font(47);
-        Font font2 = new Font( 26);
+        Font font2 = new Font(26);
         Button twoVariableRegression = new Button("Two Variable Regression");
         Button threeVariableRegression = new Button("Three Variable Regression");
         Button fourVariableRegression = new Button("Four Variable Regression");
 
-        twoVariableRegression.setPrefSize(410,230);
-        threeVariableRegression.setPrefSize(410,230);
-        fourVariableRegression.setPrefSize(410,230);
+        twoVariableRegression.setPrefSize(410, 230);
+        threeVariableRegression.setPrefSize(410, 230);
+        fourVariableRegression.setPrefSize(410, 230);
 
         twoVariableRegression.setFont(font2);
         threeVariableRegression.setFont(font2);
@@ -57,17 +56,17 @@ public class MultiVariableRegression extends Application {
         threeVariableRegression.setContentDisplay(ContentDisplay.TOP);
         fourVariableRegression.setContentDisplay(ContentDisplay.TOP);
         FileInputStream fileInputStream1 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"2var.png");
+                "src" + File.separator + "Images" + File.separator + "2var.png");
         Image image1 = new Image(fileInputStream1);
         twoVariableRegression.setGraphic(new ImageView(image1));
 
         FileInputStream fileInputStream2 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"3var.png");
+                "src" + File.separator + "Images" + File.separator + "3var.png");
         Image image2 = new Image(fileInputStream2);
         threeVariableRegression.setGraphic(new ImageView(image2));
 
         FileInputStream fileInputStream3 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"4var.png");
+                "src" + File.separator + "Images" + File.separator + "4var.png");
         Image image3 = new Image(fileInputStream3);
         fourVariableRegression.setGraphic(new ImageView(image3));
         twoVariableRegression.setOnAction(actionEvent -> {
@@ -102,7 +101,7 @@ public class MultiVariableRegression extends Application {
             System.exit(0);
         });
         FileInputStream fileInputStream4 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"exit.png");
+                "src" + File.separator + "Images" + File.separator + "exit.png");
         Image image4 = new Image(fileInputStream4);
         exit.setGraphic(new ImageView(image4));
         exit.setPrefSize(120, 20);
@@ -119,30 +118,29 @@ public class MultiVariableRegression extends Application {
         Button home = new Button("Home");
         home.setTranslateX(470);
         home.setTranslateY(520);
-        home.setPrefSize(375,30);
+        home.setPrefSize(375, 30);
         home.setFont(font2);
         home.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream5 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"home.png");
+                "src" + File.separator + "Images" + File.separator + "home.png");
         Image image5 = new Image(fileInputStream5);
         home.setGraphic(new ImageView(image5));
         home.setOnAction(actionEvent -> {
             Main main = new Main();
             try {
                 main.start(primaryStage);
-            }
-            catch (Exception exception){
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
         });
         Button back = new Button("Back");
         back.setTranslateX(950);
         back.setTranslateY(520);
-        back.setPrefSize(375,30);
+        back.setPrefSize(375, 30);
         back.setFont(font2);
         back.setContentDisplay(ContentDisplay.LEFT);
         FileInputStream fileInputStream6 = new FileInputStream(
-                "src"+ File.separator +"Images"+ File.separator +"back.png");
+                "src" + File.separator + "Images" + File.separator + "back.png");
         Image image6 = new Image(fileInputStream6);
         back.setGraphic(new ImageView(image6));
         back.setOnAction(actionEvent -> {
@@ -153,23 +151,23 @@ public class MultiVariableRegression extends Application {
                 exception.printStackTrace();
             }
         });
-        Image background = new Image("Images"+ File.separator +"framework.jpg");
+        Image background = new Image("Images" + File.separator + "framework.jpg");
         Canvas canvas = new Canvas(850, 425);
         canvas.setTranslateX(470);
         canvas.setTranslateY(35);
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
-        graphicsContext.drawImage(background,0,0);
+        graphicsContext.drawImage(background, 0, 0);
         Group group = new Group();
-        group.getChildren().addAll(canvas,twoVariableRegression,
-                threeVariableRegression,exit,back,home,text,fourVariableRegression);
-        Scene scene1 = new Scene(group,1500,950);
+        group.getChildren().addAll(canvas, twoVariableRegression,
+                threeVariableRegression, exit, back, home, text, fourVariableRegression);
+        Scene scene1 = new Scene(group, 1500, 950);
         primaryStage.setScene(scene1);
         primaryStage.setTitle("Recommendation Tool");
         primaryStage.setFullScreen(true);
         primaryStage.show();
     }
-    public Button setStyle( Button button)
-    {
+
+    public Button setStyle(Button button) {
         button.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
                 "    -fx-background-radius: 8;\n" +
@@ -181,6 +179,6 @@ public class MultiVariableRegression extends Application {
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 2.1em;");
-        return  button;
+        return button;
     }
 }
