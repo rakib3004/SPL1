db0e32690b186422a04c194c9f8dcd24a6c09daf
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:57:16 +0600

    page-rank-matrix-calculation.java refactoring

--- src/PageRankAlgorithm/PageRankMatrixCalculation.java
+++ src/PageRankAlgorithm/PageRankMatrixCalculation.java
@@ -3,24 +3,26 @@ package PageRankAlgorithm;
 import FilePackage.DateTimeWriter;
 
 public class PageRankMatrixCalculation {
-    int iterator,jterator;
-    public double [] pageRankMatrixCalculationMethods(double [] pageWeight, double [][] pageRankMatrix, int numberOfBooks){
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+    int iterator, jterator;
+
+    public double[] pageRankMatrixCalculationMethods(double[] pageWeight, double[][] pageRankMatrix,
+            int numberOfBooks) {
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
-        double [] weightSum = new double[numberOfBooks];
+        double[] weightSum = new double[numberOfBooks];
 
-        for(iterator=0;iterator<numberOfBooks;iterator++){
-                weightSum[iterator] =0.0;
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            weightSum[iterator] = 0.0;
         }
-        for(iterator=0;iterator<numberOfBooks;iterator++){
-            for(jterator=0;jterator<numberOfBooks;jterator++){
-weightSum[iterator] = weightSum[iterator]+ (pageRankMatrix[iterator][jterator]*pageWeight[jterator]);
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            for (jterator = 0; jterator < numberOfBooks; jterator++) {
+                weightSum[iterator] = weightSum[iterator] + (pageRankMatrix[iterator][jterator] * pageWeight[jterator]);
 
             }
         }
-        pageWeight =weightSum;
+        pageWeight = weightSum;
         return pageWeight;
     }
 }
