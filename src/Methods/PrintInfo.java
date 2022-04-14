4f7e4a906ca63dcc2c39452a1042779242c31c53
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:39:58 +0600

    print-info.java refactoring

--- src/Methods/PrintInfo.java
+++ src/Methods/PrintInfo.java
@@ -5,11 +5,12 @@ import ObjectOriented.BookData;
 import ObjectOriented.PriorityData;
 
 public class PrintInfo {
-int iterator;
-int jterator;
+    int iterator;
+    int jterator;
+
     public void printInfoMethods(BookData[] bookData, int numberOfBooks) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         for (jterator = 0; jterator < numberOfBooks; jterator++) {
@@ -28,9 +29,10 @@ int jterator;
             System.out.println(bookData[jterator].bookPrice);
         }
     }
+
     public void showMLRProcessResult(PriorityData[] priorityData, int numberOfBooks) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         System.out.println("Book Name :\t Writer Name : \t Weight ");
         for (iterator = 0; iterator < numberOfBooks; iterator++) {
@@ -38,28 +40,29 @@ int jterator;
                     + priorityData[iterator].bookData.writerName + "\t" + priorityData[iterator].getMLRweight());
         }
     }
+
     public void showAHPProcessResult(PriorityData[] priorityData, int numberOfBooks) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
 
         System.out.println("Book Name :\t Writer Name : \t Weight ");
-        for(iterator=0;iterator<numberOfBooks;iterator++){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
 
-            System.out.println(priorityData[iterator].bookData.bookName+"\t"
-                    +priorityData[iterator].bookData.writerName+"\t"+priorityData[iterator].getAHPweight());
+            System.out.println(priorityData[iterator].bookData.bookName + "\t"
+                    + priorityData[iterator].bookData.writerName + "\t" + priorityData[iterator].getAHPweight());
         }
     }
 
     public void showPRAProcessResult(PriorityData[] priorityData, int numberOfBooks) {
-        String  className = this.getClass().getSimpleName();
-        DateTimeWriter dateTimeWriter =  new DateTimeWriter();
+        String className = this.getClass().getSimpleName();
+        DateTimeWriter dateTimeWriter = new DateTimeWriter();
         dateTimeWriter.dateTimeWriterMethods(className);
         System.out.println("Book Name :\t Writer Name : \t Weight ");
-        for(iterator=0;iterator<numberOfBooks;iterator++){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
 
-            System.out.println(priorityData[iterator].bookData.bookName+"\t"
-                    +priorityData[iterator].bookData.writerName+"\t"+priorityData[iterator].getPRAweight());
+            System.out.println(priorityData[iterator].bookData.bookName + "\t"
+                    + priorityData[iterator].bookData.writerName + "\t" + priorityData[iterator].getPRAweight());
         }
     }
 }
