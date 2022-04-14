6f5ca1091063c5bbff88613184874f086ea29e62
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 23:57:31 +0600

    page-rank-process-data.java refactoring

--- src/PageRankAlgorithm/PageRankProcessData.java
+++ src/PageRankAlgorithm/PageRankProcessData.java
@@ -6,22 +6,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 public class PageRankProcessData {
-    String [] stringArray = new String[632];
-    public PriorityData[]  PageRankProcessDataMethods(PriorityData[] priorityData, int numberOfBooks) throws IOException {
-int iterator;
- double [] pageRankArray  = new double[632];
-        for(iterator=0;iterator<numberOfBooks;iterator++){
-stringArray[iterator]="";
+    String[] stringArray = new String[632];
+
+    public PriorityData[] PageRankProcessDataMethods(PriorityData[] priorityData, int numberOfBooks)
+            throws IOException {
+        int iterator;
+        double[] pageRankArray = new double[632];
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            stringArray[iterator] = "";
         }
         pageRankArray = pageRankDataFromText(pageRankArray);
-for(iterator=0;iterator<numberOfBooks;iterator++){
-    priorityData[iterator].setPRAweight(pageRankArray[iterator]);
-    priorityData[iterator].setRankValue(pageRankArray[iterator]);
-}
-        return  priorityData;
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
+            priorityData[iterator].setPRAweight(pageRankArray[iterator]);
+            priorityData[iterator].setRankValue(pageRankArray[iterator]);
+        }
+        return priorityData;
     }
-    public double [] pageRankDataFromText(double[] pageRankArray) throws IOException {
+
+    public double[] pageRankDataFromText(double[] pageRankArray) throws IOException {
         int iterator;
 
         String FILENAME = "PageRankAlgo.txt";
@@ -30,28 +34,27 @@ for(iterator=0;iterator<numberOfBooks;iterator++){
         char[] arrayOfCharacter = new char[900000];
         fileReader.read(arrayOfCharacter);
         int iCount;
-        int charIndex=0;
-        int wordIndex=0;
-int iterator1=0;
+        int charIndex = 0;
+        int wordIndex = 0;
+        int iterator1 = 0;
         for (iCount = 0; arrayOfCharacter[iCount] != '\0'; iCount++) {
             if (arrayOfCharacter[iCount] == '\n') {
                 // iCount++;
                 stringArray[iterator1] = stringArray[iterator1].trim();
                 pageRankArray[iterator1] = Double.parseDouble(stringArray[iterator1]);
                 iterator1++;
-            }
-            else{
-                stringArray[iterator1] = stringArray[iterator1]+arrayOfCharacter[iCount];
+            } else {
+                stringArray[iterator1] = stringArray[iterator1] + arrayOfCharacter[iCount];
             }
         }
         fileReader.close();
         return pageRankArray;
     }
-    public void printPageRankAlgorithmDataView(PriorityData [] priorityData, int numberOfBooks){
+
+    public void printPageRankAlgorithmDataView(PriorityData[] priorityData, int numberOfBooks) {
         int iterator;
-        for(iterator=0;iterator<numberOfBooks;iterator++){
+        for (iterator = 0; iterator < numberOfBooks; iterator++) {
             System.out.println(priorityData[iterator].getPRAweight());
         }
     }
-    }
-
+}
